4e79cdf98e782a14058c0f6c89c26da770af58cf
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-06-30 21:39:10 +0200

    Initial main

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
+import { VueQueryPlugin } from '@tanstack/vue-query';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+const API_PORT = '8000';
+const API_HOST = 'http://localhost';
+export const API_URL = `${API_HOST}:${API_PORT}`;
 
-app.mount('#app')
+app.use(createPinia());
+app.use(router);
+app.use(VueQueryPlugin);
+app.mount('#app');

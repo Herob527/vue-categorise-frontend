393ea8581d31ecdac0238f573134be70d07ff856
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-07-01 22:41:35 +0200

    Main page redirect to Startup project if nothing is in DB, else it'll redirect to Transcript
    TODO

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,4 +1,32 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { axiosApi } from '@/axiosConfig';
+import { API_URL } from '@/constants';
+import router from '@/router';
+import { useQuery } from '@tanstack/vue-query';
+import { onUpdated } from 'vue';
+
+const queryFn = async () => {
+  const res = await axiosApi.get(`${API_URL}/bindings/count`);
+  return res.data;
+};
+
+const { data, isLoading, isError } = useQuery({
+  queryKey: ['bindingsCount'],
+  queryFn,
+});
+
+onUpdated(() => {
+  const count = data.value;
+  if (count === 0) {
+    router.replace('/startup');
+  } else if (count > 0) {
+    router.replace('/transcription');
+  }
+});
+</script>
+
 <template>
-  <p>Hejo</p>
+  <span v-if="isLoading">Loading...</span>
+  <span v-else-if="isError">Error!</span>
+  <span v-else>{{ data }}</span>
 </template>

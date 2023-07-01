fb7fcec95d84e00a51a8201fa317eb22ec2979f0
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-07-01 22:40:43 +0200

    Moved api url to constants

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+const API_PORT = '8000';
+const API_HOST = 'http://localhost';
+
+export const API_URL = `${API_HOST}:${API_PORT}`;
--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,6 @@ import { VueQueryPlugin } from '@tanstack/vue-query';
 
 const app = createApp(App);
 
-const API_PORT = '8000';
-const API_HOST = 'http://localhost';
-
-export const API_URL = `${API_HOST}:${API_PORT}`;
 app.use(createPinia());
 app.use(router);
 app.use(VueQueryPlugin);

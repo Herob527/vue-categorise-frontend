a05a8bb7e51aa2328c81c244f0d22363e1a07ee6
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-07-02 12:16:28 +0200

    Added listing categories

---
+++ src/components/CategoryList.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useCategoriesStore } from '@/stores/categories';
+
+const { useGetAllCategories } = useCategoriesStore();
+const { data, isSuccess } = useGetAllCategories();
+</script>
+
+<template>
+  <select v-if="isSuccess">
+    <option v-for="category in data" :key="category.name">
+      {{ category.name }}
+    </option>
+  </select>
+</template>

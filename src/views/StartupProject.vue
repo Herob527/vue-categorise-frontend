9f063680e1bd6a84f72e4168da33d96c59a7d2c3
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-07-02 12:17:19 +0200

    Added basics for displaying existing categories

--- src/views/StartupProject.vue
+++ src/views/StartupProject.vue
@@ -1,6 +1,14 @@
 <script setup lang="ts">
-console.log('hejo');
+import CategoryEntry from '@/components/Startup/CategoryEntry.vue';
+import { useCategoriesStore } from '@/stores/categories';
+const { useGetAllCategories } = useCategoriesStore();
+const { data, isLoading, isSuccess } = useGetAllCategories();
 </script>
 <template>
-  <p>Start</p>
+  <div v-if="isLoading">Loading...</div>
+  <div v-else-if="isSuccess" class="container mx-auto">
+    <div v-for="category in data" :key="category.name">
+      <CategoryEntry :category="category" />
+    </div>
+  </div>
 </template>

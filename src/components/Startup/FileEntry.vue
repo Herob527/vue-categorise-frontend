1c2352e4e880dd6ef989dbff2bbd2047b6d03f2f
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-07-02 13:07:19 +0200

    Added props for file entry

--- src/components/Startup/FileEntry.vue
+++ src/components/Startup/FileEntry.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
+import type { fileEntry } from '@/stores/audioFiles';
+
 console.log('hejo');
+type propTypes = {
+  audio: fileEntry;
+};
+const props = defineProps<propTypes>();
 </script>
 <template>
-  <p>Hejo</p>
+  <p>{{ audio.file.name }}</p>
 </template>

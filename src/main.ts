2890383094bcb9c2141fa059a6c8304705362eff
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-07-03 19:38:29 +0200

    Added font-awesome to vue

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,18 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 import { VueQueryPlugin } from '@tanstack/vue-query';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library, config } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
 const app = createApp(App);
+
+library.add(fas);
+
+config.styleDefault = 'solid';
 
 app.use(createPinia());
 app.use(router);
 app.use(VueQueryPlugin);
+app.component('font-awesome-icon', FontAwesomeIcon);
 app.mount('#app');

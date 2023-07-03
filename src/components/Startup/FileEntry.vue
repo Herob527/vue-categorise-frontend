e24a2b572e44b6ec16c763a8192e83c64e865262
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-07-03 19:47:34 +0200

    Adding removing each entry

--- src/components/Startup/FileEntry.vue
+++ src/components/Startup/FileEntry.vue
@@ -1,38 +1,28 @@
 <script setup lang="ts">
-import type { fileEntry } from '@/stores/audioFiles';
+import { useAudioFilesStore, type fileEntry } from '@/stores/audioFiles';
 import { convert } from 'convert';
-import { ref } from 'vue';
 
 type propTypes = {
   audio: fileEntry;
 };
-const length = ref(0);
-const getDuration = async (file: fileEntry['file']) => {
-  const reader = new FileReader();
-  reader.readAsArrayBuffer(file);
-  reader.onloadend = async (e) => {
-    const ctx = new AudioContext();
-    if (!e.target) return;
-    const audioArrayBuffer = e.target.result as ArrayBuffer;
-    return await ctx.decodeAudioData(
-      audioArrayBuffer,
-      (data) => {
-        length.value = Math.round(data.duration * 100) / 100;
-      },
-      (error) => {
-        return null;
-      },
-    );
-  };
-};
 const props = defineProps<propTypes>();
 const bestUnit = convert(props.audio.file.size, 'B').to('best').toString(2);
-getDuration(props.audio.file);
+const { remove } = useAudioFilesStore();
+const handleRemoveAudio = () => {
+  remove(props.audio.id);
+};
 </script>
 <template>
   <div class="grid grid-cols-5 gap-2 text-center">
     <span>{{ audio.file.name }}</span>
     <span>{{ bestUnit }}</span>
-    <span>{{ length === 0 ? 'Calculating length...' : `${length} s` }} </span>
+
+    <button
+      @click="handleRemoveAudio"
+      class="flex flex-col col-start-5 col-end-5 justify-center justify-self-end items-center p-1 w-8 h-8 text-white bg-red-500 rounded-md hover:bg-red-700"
+      title="Remove audio"
+    >
+      <font-awesome-icon icon="fa-xmark" />
+    </button>
   </div>
 </template>

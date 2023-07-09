96bb98e6726ea2753c3bf7557e1cb9646b9f0e7a
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-07-09 13:58:23 +0200

    Fixing status indicator

--- src/components/Startup/StatusIndicator.vue
+++ src/components/Startup/StatusIndicator.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
 import { type fileEntry } from '@/stores/audioFiles';
+import { ref } from 'vue';
 
 type propTypes = {
   status: fileEntry['status'];
 };
-
 const props = defineProps<propTypes>();
-console.log(props);
 const colors: Record<(typeof props)['status'], string> = {
   done: 'bg-green-500',
   error: 'bg-red-500',
@@ -14,13 +13,11 @@ const colors: Record<(typeof props)['status'], string> = {
   onServer: 'bg-yellow-500',
   processing: 'bg-orange-500',
 };
-const currentColor = colors[props.status];
-console.log(currentColor);
 </script>
 <template>
   <div class="flex gap-2 justify-center items-center">
     <span
-      :class="currentColor"
+      :class="colors[status]"
       class="inline-block w-4 h-4 rounded-full"
     ></span>
     <span>{{ status }}</span>

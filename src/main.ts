b8a1cba75d3dd43fafd4b2aaa8655a15cd8cafd7
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-07-11 21:56:28 +0200

    Added vue virtual scroller

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-
+import 'vue-virtual-scroller/dist/vue-virtual-scroller.css';
 import App from './App.vue';
 import router from './router';
 import { VueQueryPlugin } from '@tanstack/vue-query';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { library, config } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
+import VueVirtualScroller from 'vue-virtual-scroller';
 
 const app = createApp(App);
 
+app.use(VueVirtualScroller);
 library.add(fas);
 
 config.styleDefault = 'solid';

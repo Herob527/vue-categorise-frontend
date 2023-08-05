9009d5b2d2defdc73e88b01072f34ef09aabe114
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-08-05 23:19:43 +0200

    Populated bindings actions

---
+++ src/hooks/actions/bindings.ts
@@ -0,0 +1,36 @@
+import { axiosApi } from '@/axiosConfig';
+
+export type postBindingType = {
+  audio: File;
+  category: string;
+};
+
+export type UUID = string;
+
+export const post = async ({ audio, category }: postBindingType) => {
+  const formData = new FormData();
+  formData.set('audio', audio);
+  formData.set('category', category);
+  const res = await axiosApi.postForm('bindings', formData);
+  return res.data;
+};
+
+export const getAll = async () => {
+  const res = await axiosApi.get('bindings/all');
+  return res.data;
+};
+
+export const getOne = async ({ id }: { id: UUID }) => {
+  const res = await axiosApi.get(`bindings/${id}`);
+  return res.data;
+};
+
+export const deleteOne = async ({ id }: { id: UUID }) => {
+  const res = await axiosApi.delete(`bindings/${id}`);
+  return res.data;
+};
+
+export const getCount = async () => {
+  const res = await axiosApi.get('bindings/count');
+  return res.data;
+};
--- src/hooks/useBindings.ts
+++ src/hooks/useBindings.ts
@@ -1,42 +1,48 @@
-import { axiosApi } from '@/axiosConfig';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/vue-query';
 import type { AxiosError } from 'axios';
-
-type postBindingType = {
-  audio: File;
-  category: string;
-};
-
-const postBinding = async ({ audio, category }: postBindingType) => {
-  const formData = new FormData();
-  formData.set('audio', audio);
-  formData.set('category', category);
-  const res = await axiosApi.postForm('bindings', formData);
-  return res.data;
-};
-
-const getAllBindings = async () => {
-  const res = await axiosApi.get('bindings/all');
-  return res.data;
-};
+import {
+  deleteOne,
+  getAll,
+  getOne,
+  post,
+  type UUID,
+  type postBindingType,
+} from './actions/bindings';
 
 const useBindings = () => {
   const queryClient = useQueryClient();
 
-  const usePostBinding = () =>
+  const usePost = () =>
     useMutation<unknown, AxiosError, postBindingType>({
-      mutationFn: ({ audio, category }) => postBinding({ audio, category }),
+      mutationFn: async ({ audio, category }) => post({ audio, category }),
       mutationKey: ['postBinding'],
       onError: (err) => {
         return err;
       },
+      onSuccess: () => {
+        queryClient.invalidateQueries(['getAllBindings']);
+      },
     });
-  const useGetAllBindings = () =>
+
+  const useDelete = () =>
+    useMutation<unknown, AxiosError, { id: UUID }>({
+      mutationFn: ({ id }) => deleteOne({ id }),
+      onSuccess: (_, { id }) => {
+        queryClient.invalidateQueries(['getAllBindings']);
+        queryClient.invalidateQueries(['getBinding', id]);
+      },
+    });
+
+  const useGetAll = () =>
     useQuery({
-      queryFn: () => getAllBindings(),
+      queryFn: () => getAll(),
       queryKey: ['getAllBindings'],
     });
-  return { usePostBinding, useGetAllBindings };
+
+  const useGetOne = ({ id }: { id: UUID }) =>
+    useQuery({ queryKey: ['getBinding', id], queryFn: () => getOne({ id }) });
+
+  return { usePost, useGetAll, useDelete, useGetOne };
 };
 
 export { useBindings };

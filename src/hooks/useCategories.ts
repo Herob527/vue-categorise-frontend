8fe58619af335ee1fe023520c6d5a7b4ec8e5e12
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-08-06 21:13:49 +0200

    Added queries hooks for useCategories

---
+++ src/hooks/useCategories.ts
@@ -0,0 +1,36 @@
+import { useMutation, useQuery, useQueryClient } from '@tanstack/vue-query';
+import { getAll, deleteOne, updateOne, post } from '@/actions/categories';
+import type { AxiosError } from 'axios';
+
+export const useCategories = () => {
+  const queryClient = useQueryClient();
+
+  const usePost = () =>
+    useMutation<unknown, AxiosError, { id: string; name: string }>({
+      mutationFn: async ({ id, name }) => post({ id, name }),
+      onSuccess: () => {
+        queryClient.invalidateQueries({ queryKey: ['categories'] });
+      },
+    });
+  const useUpdate = () =>
+    useMutation<unknown, AxiosError, { id: string; name: string }>({
+      mutationFn: async ({ id, name }) => updateOne({ id, name }),
+      onSuccess: () => {
+        queryClient.invalidateQueries({ queryKey: ['categories'] });
+      },
+    });
+  const useDeleteOne = () =>
+    useMutation<unknown, AxiosError, { id: string }>({
+      mutationFn: async ({ id }) => deleteOne({ id }),
+      onSuccess: () => {
+        queryClient.invalidateQueries({ queryKey: ['categories'] });
+      },
+    });
+  const useGetAll = () =>
+    useQuery({
+      queryKey: ['categories'],
+      queryFn: async () => getAll(),
+    });
+
+  return { usePost, useUpdate, useDeleteOne, useGetAll };
+};

a9d74c36b9f5d72a503b0219dfb8065a44d336a1
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-11-30 21:23:43 +0100

    Added handler for error

--- src/views/StartupProject.vue
+++ src/views/StartupProject.vue
@@ -28,21 +28,36 @@ const getAudio = async (url: string) => {
 };
 onUpdated(async () => {
   if (!bindings.value) return;
-  const mappedData = (bindings.value as any[]).map(async (b) => {
-    const { Bindings, Categories, Audios } = b;
-    const audio = await getAudio(Audios.url);
-    const audioFile = new File([audio.data], Audios.file_name, {
-      type: audio.data.type,
+  try {
+    const mappedData = (bindings.value as any[]).map(async (b) => {
+      try {
+        const { Bindings, Categories, Audios } = b;
+        const audio = await getAudio(Audios.url);
+        const audioFile = new File([audio.data], Audios.file_name, {
+          type: audio.data.type,
+        });
+        const bindingsId: string = Bindings.id;
+        const category: string = Categories.name;
+        return { audioFile, category, bindingsId };
+      } catch (e) {
+        console.error(e);
+        return {
+          audioFile: new File([], '', {
+            type: 'null',
+          }),
+          category: '',
+          bindingsId: null,
+        };
+      }
     });
-    const bindingsId: string = Bindings.id;
-    const category: string = Categories.name;
-    return { audioFile, category, bindingsId };
-  });
-  const resolved = await Promise.all(mappedData);
-  resolved.forEach((el) => {
-    const id = generateId(el.audioFile);
-    add(el.audioFile, id, el.category, 'onServer');
-  });
+    const resolved = await Promise.all(mappedData);
+    resolved.forEach((el) => {
+      const id = generateId(el.audioFile);
+      add(el.audioFile, id, el.category, 'onServer');
+    });
+  } catch (e) {
+    console.log(e);
+  }
 });
 </script>
 <template>

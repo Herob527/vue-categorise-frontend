1d33b35635ce2088ea64d9f5b51d2947dc433adc
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-11-30 21:23:24 +0100

    Added more decent header

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from 'vue-router';
 </script>
 
 <template>
-  <header>
-    <p class="text-red-900">Hejo</p>
+  <header class="container p-4 mx-auto">
+    <h1 class="text-blue-500 text-2xl text-center font-bold uppercase">
+      App or something
+    </h1>
   </header>
 
   <RouterView />

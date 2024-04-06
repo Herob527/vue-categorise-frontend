5a2e866974cbf3b5c62c947beb8479bab7348898
Herob527 <szymon.wrzos@onet.pl>
2024-04-06 13:53:11 +0200

    Reworking logic

---
+++ src/components/Startup/AudioList.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { getAll } from '@/actions/bindings';
+import { useQuery } from '@tanstack/vue-query';
+
+const { data } = useQuery({
+  queryKey: ['bindings'],
+  queryFn: async () => getAll(),
+});
+</script>
+
+<template>
+  <ol v-for="item in data" v-bind:key="item.bindings.id">
+    <li>{{ item.categories }}</li>
+  </ol>
+</template>

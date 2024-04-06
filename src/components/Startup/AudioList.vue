3555187ad2aecac42f83e99a38ee4beec0a91f2b
Herob527 <szymon.wrzos@onet.pl>
2024-04-07 00:18:03 +0200

    Changed to table - temporarily

--- src/components/Startup/AudioList.vue
+++ src/components/Startup/AudioList.vue
@@ -10,11 +10,20 @@ onMounted(() => {
 </script>
 
 <template>
-  <ol v-for="item in store.entries" v-bind:key="item.id">
-    <li>
-      <span>{{ item.filename }}</span>
-      <span>{{ item.duration }}</span>
-      <span> {{ item.status }} </span>
-    </li>
-  </ol>
+  <table
+    v-for="[index, item] in store.entries.entries()"
+    v-bind:key="item.id"
+    class="text-center border-collapse border-spacing-2"
+  >
+    <tr v-if="index === 0" class="bg-gray-100">
+      <td class="py-2 px-4">Title</td>
+      <td class="py-2 px-4">Duration</td>
+      <td class="py-2 px-4">Status</td>
+    </tr>
+    <tr>
+      <td class="py-2 px-4">{{ item.filename }}</td>
+      <td class="py-2 px-4">{{ item.duration }}</td>
+      <td class="py-2 px-4">{{ item.status }}</td>
+    </tr>
+  </table>
 </template>

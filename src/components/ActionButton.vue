07f482613a4b7da4bb2049f2815ec5bf66bf082b
Herob527 <szymon.wrzos@onet.pl>
2024-04-09 00:41:29 +0200

    Added action button

---
+++ src/components/ActionButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const props = defineProps<{
+  onClick: () => void;
+  label?: string;
+  className?: string;
+}>();
+</script>
+
+<template>
+  <button
+    @click="props.onClick()"
+    type="button"
+    :title="label"
+    :class="className"
+  >
+    <slot></slot>
+    <!-- This is where the child content will be inserted -->
+  </button>
+</template>

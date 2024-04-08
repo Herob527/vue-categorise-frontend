953926265f46de837b42b1a616028acf6cb392cf
Herob527 <szymon.wrzos@onet.pl>
2024-04-09 00:41:43 +0200

    Made use of status icon and action button

--- src/components/Startup/AudioList.vue
+++ src/components/Startup/AudioList.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import { useBindingsStore } from '@/stores/bindingsStore';
 import { onMounted } from 'vue';
+import ActionButton from '@/components/ActionButton.vue';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import StatusIcon from '../StatusIcon.vue';
 
 const store = useBindingsStore();
 
@@ -17,13 +20,37 @@ onMounted(() => {
   >
     <tr v-if="index === 0" class="bg-gray-100">
       <td class="py-2 px-4">Title</td>
-      <td class="py-2 px-4">Duration</td>
+      <td class="py-2 px-4">Duration (s)</td>
       <td class="py-2 px-4">Status</td>
+      <td class="py-2 px-4">Actions</td>
     </tr>
     <tr>
       <td class="py-2 px-4">{{ item.filename }}</td>
       <td class="py-2 px-4">{{ item.duration }}</td>
-      <td class="py-2 px-4">{{ item.status }}</td>
+      <td class="relative py-2 px-4"><StatusIcon status="inDatabase" /></td>
+      <td class="flex flex-row gap-2 py-2 px-4">
+        <ActionButton
+          :on-click="() => console.log('clicked')"
+          class-name="bg-red-500 text-white px-4 py-4 relative rounded-md hover:bg-red-700"
+          label="Delete"
+        >
+          <FontAwesomeIcon
+            icon="fa-solid fa-trash"
+            class="absolute top-1/2 left-1/2 w-1/2 h-1/2 text-white -translate-x-1/2 -translate-y-1/2"
+          />
+        </ActionButton>
+
+        <ActionButton
+          :on-click="() => console.log('clicked')"
+          class-name="bg-blue-500 text-white px-4 py-4 relative rounded-md hover:bg-blue-700"
+          label="Added"
+        >
+          <FontAwesomeIcon
+            icon="fa-solid fa-plus"
+            class="absolute top-1/2 left-1/2 w-1/2 h-1/2 text-white -translate-x-1/2 -translate-y-1/2"
+          />
+        </ActionButton>
+      </td>
     </tr>
   </table>
 </template>

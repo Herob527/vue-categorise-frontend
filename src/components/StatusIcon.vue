ddd06a2dad71b067b74e7a680d5c695a4cb7ae26
Herob527 <szymon.wrzos@onet.pl>
2024-04-09 00:41:33 +0200

    Added status icon

---
+++ src/components/StatusIcon.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { statuses } from '@/types/shared';
+
+const { status } = defineProps<{ status: `${statuses}` }>();
+
+const colors: Record<`${statuses}`, string> = {
+  error: 'red',
+  pending: 'yellow',
+  processing: 'blue',
+  inDatabase: 'orange',
+};
+</script>
+<template>
+  <span
+    :style="`background-color: ${colors[status] || colors.inDatabase}`"
+    class="absolute top-1/2 left-1/2 py-3 px-3 rounded-full -translate-x-1/2 -translate-y-1/2"
+    :title="status"
+  ></span>
+</template>

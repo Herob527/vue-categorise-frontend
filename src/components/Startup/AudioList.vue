dd7d8f258cfd61340629457fba9d231c754608f8
Herob527 <szymon.wrzos@onet.pl>
2024-04-09 23:33:37 +0200

    Moved from table to div and did some lifting

--- src/components/Startup/AudioList.vue
+++ src/components/Startup/AudioList.vue
@@ -13,30 +13,39 @@ onMounted(() => {
 </script>
 
 <template>
-  <table
+  <div
     v-for="[index, item] in store.entries.entries()"
     v-bind:key="item.id"
-    class="container mx-auto w-full text-center border-collapse border-spacing-2"
+    class="container mx-auto w-full text-center rounded-lg border-2 border-gray-400 overflow-clip"
   >
-    <tr v-if="index === 0" class="bg-gray-100">
-      <th class="py-2 px-4">Title</th>
-      <th class="py-2 px-4">Duration (s)</th>
-      <th class="py-2 px-4">Status</th>
-      <th class="py-2 px-4">Actions</th>
-    </tr>
-    <tr>
-      <td class="py-2 px-4">
-        <span class="px-1 bg-gray-200 hover:bg-gray-300" :title="item.filename">
+    <div v-if="index === 0" class="bg-gray-100">
+      <div class="flex justify-between text-xl font-bold">
+        <div class="flex-1 py-2 px-4">Title</div>
+        <div class="flex-1 py-2 px-4">Duration (s)</div>
+        <div class="flex-1 py-2 px-4">Status</div>
+        <div class="flex-1 py-2 px-4">Actions</div>
+      </div>
+    </div>
+    <div class="flex justify-between">
+      <div class="flex flex-col flex-1 justify-center items-center py-2 px-4">
+        <span
+          class="py-1 px-2 bg-gray-200 cursor-text hover:bg-gray-300"
+          :title="item.filename"
+        >
           {{
             item.filename.length > 15
               ? item.filename.slice(0, 15) + '...'
               : item.filename
           }}
         </span>
-      </td>
-      <td class="py-2 px-4">{{ item.duration }}</td>
-      <td class="relative py-2 px-4"><StatusIcon status="inDatabase" /></td>
-      <td class="flex flex-row gap-2 justify-center items-center py-2 px-4">
+      </div>
+      <div class="flex-1 py-2 px-4">{{ item.duration }}</div>
+      <div class="relative flex-1 py-2 px-4">
+        <StatusIcon status="inDatabase" />
+      </div>
+      <div
+        class="flex flex-row flex-1 gap-2 justify-center items-center py-2 px-4"
+      >
         <ActionButton
           :on-click="() => console.log('clicked')"
           class-name="bg-red-500 text-white px-4 py-4 relative rounded-md hover:bg-red-700"
@@ -51,14 +60,14 @@ onMounted(() => {
         <ActionButton
           :on-click="() => console.log('clicked')"
           class-name="bg-blue-500 text-white px-4 py-4 relative rounded-md hover:bg-blue-700"
-          label="Added"
+          label="Add"
         >
           <FontAwesomeIcon
             icon="fa-solid fa-plus"
             class="absolute top-1/2 left-1/2 w-1/2 h-1/2 text-white -translate-x-1/2 -translate-y-1/2"
           />
         </ActionButton>
-      </td>
-    </tr>
-  </table>
+      </div>
+    </div>
+  </div>
 </template>

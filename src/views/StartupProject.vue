d3bb58f8478816eb14c23d4403bb96d94de49940
Herob527 <szymon.wrzos@onet.pl>
2024-04-09 23:33:51 +0200

    Added table action panel

--- src/views/StartupProject.vue
+++ src/views/StartupProject.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import AudioList from '@/components/Startup/AudioList.vue';
+import TableActionPanel from '@/components/Startup/TableActionPanel.vue';
 </script>
 <template>
+  <TableActionPanel />
   <AudioList />
 </template>

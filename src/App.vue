b37122d44fe5504c286f4faee888454a98a258f9
Herob527 <szymon.wrzos@onet.pl>
2024-04-14 23:44:11 +0200

    Removed header

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,5 @@ import { RouterLink, RouterView } from 'vue-router';
 </script>
 
 <template>
-  <header class="container p-4 mx-auto">
-    <h1 class="text-blue-500 text-2xl text-center font-bold uppercase">
-      App or something
-    </h1>
-  </header>
-
   <RouterView />
 </template>

bb501df7c50a2112db78cd030b6bbf909d94634c
Herob527 <szymon.wrzos@onet.pl>
2024-04-14 23:44:20 +0200

    Added going back to main menu

--- src/views/StartupProject.vue
+++ src/views/StartupProject.vue
@@ -1,8 +1,24 @@
 <script setup lang="ts">
+import ActionButton from '@/components/ActionButton.vue';
 import AudioList from '@/components/Startup/AudioList.vue';
 import TableActionPanel from '@/components/Startup/TableActionPanel.vue';
+import { useRouter } from 'vue-router';
+const router = useRouter();
+
+const handleClick = () => {
+  router.replace({ name: 'home' });
+};
 </script>
 <template>
+  <nav class="container my-4 mx-auto">
+    <ActionButton
+      :onClick="handleClick"
+      class-name="bg-blue-500 text-white px-4 py-2 rounded-xl"
+    >
+      <font-awesome-icon icon="fa-solid fa-arrow-left" />
+      Go home
+    </ActionButton>
+  </nav>
   <TableActionPanel />
   <AudioList />
 </template>

8fd1b5b42db1ce1cda02823cbee1669437356714
Herob527 <szymon.wrzos@onet.pl>
2024-04-14 23:44:39 +0200

    Added virtual scroll

--- src/components/Startup/AudioList.vue
+++ src/components/Startup/AudioList.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import 'vue-virtual-scroller/dist/vue-virtual-scroller.css';
 import { useBindingsStore } from '@/stores/bindingsStore';
 import AudioItem from './AudioItem.vue';
 import { onMounted } from 'vue';
+import { DynamicScroller, DynamicScrollerItem } from 'vue-virtual-scroller';
 
 const store = useBindingsStore();
 
@@ -14,11 +16,12 @@ onMounted(() => {
   <div
     class="container mx-auto rounded-lg border-2 border-gray-400 overflow-clip"
   >
-    <AudioItem
-      v-for="[index, item] in store.entries.entries()"
-      v-bind:key="item.id"
-      :index="index"
-      :entry="item"
-    ></AudioItem>
+    <DynamicScroller :items="store.entries" :min-item-size="75">
+      <template v-slot="{ item, index, active }">
+        <DynamicScrollerItem :item="item" :data-index="index" :active="active">
+          <AudioItem :key="item.id" :index="index" :entry="item"></AudioItem>
+        </DynamicScrollerItem>
+      </template>
+    </DynamicScroller>
   </div>
 </template>

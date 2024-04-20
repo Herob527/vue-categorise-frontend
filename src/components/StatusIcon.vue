c4a531a2841181e334e535d02e86d74e13ccb966
Herob527 <szymon.wrzos@onet.pl>
2024-04-20 15:57:56 +0200

    Made status optional

--- src/components/StatusIcon.vue
+++ src/components/StatusIcon.vue
@@ -2,11 +2,11 @@
 import type { statuses } from '@/types/shared';
 import { STATUS_COLORS } from '@/constants';
 
-const { status } = defineProps<{ status: `${statuses}` }>();
+const { status } = defineProps<{ status?: `${statuses}` }>();
 </script>
 <template>
   <span
-    :style="`background-color: ${STATUS_COLORS[status] || STATUS_COLORS.inDatabase}`"
+    :style="`background-color: ${status ? STATUS_COLORS[status] : STATUS_COLORS.inDatabase}`"
     class="absolute top-1/2 left-1/2 py-3 px-3 rounded-full -translate-x-1/2 -translate-y-1/2"
     :title="status"
   ></span>

f443c61942d47d26da6fc6f23e8c4f41058b60d4
Herob527 <szymon.wrzos@onet.pl>
2024-04-20 15:58:23 +0200

    Refactored of data table so it's easier to give labels and title

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,8 +1,29 @@
 <script setup lang="ts" generic="T">
-const props = defineProps<{ data: T[]; className?: string }>();
+const props = defineProps<{
+  data: T[];
+  className?: string;
+  title?: string;
+  itemKeys?: string[];
+}>();
 </script>
 <template>
-  <slot name="fallback" v-if="props.data.length === 0" />
+  <div
+    v-if="props.data.length === 0"
+    class="flex overflow-hidden flex-col rounded-xl border-2 border-primary-500"
+  >
+    <p class="p-2 text-2xl font-bold text-white uppercase bg-primary-600">
+      {{ props.title }}
+    </p>
+    <div
+      class="flex flex-row items-center py-2 text-xl font-bold text-center text-white uppercase bg-primary-500"
+    >
+      <span class="flex-1" v-for="item in itemKeys" :key="item">{{
+        item
+      }}</span>
+    </div>
+    <slot name="fallback" v-if="props.data.length === 0" />
+  </div>
+
   <DynamicScroller
     :items="data"
     :min-item-size="75"
@@ -16,7 +37,18 @@ const props = defineProps<{ data: T[]; className?: string }>();
         :active="active"
         v-if="typeof index === 'number'"
       >
-        <slot name="heading" v-if="index === 0" />
+        <div v-if="index === 0" class="flex flex-col">
+          <p class="p-2 text-2xl font-bold text-white uppercase bg-primary-600">
+            {{ props.title }}
+          </p>
+          <div
+            class="flex flex-row items-center py-2 text-xl font-bold text-center text-white uppercase bg-primary-500"
+          >
+            <span class="flex-1" v-for="item in itemKeys" :key="item">{{
+              item
+            }}</span>
+          </div>
+        </div>
         <slot name="item" :key="item.id" :index="index" :entry="item" />
       </DynamicScrollerItem>
     </template>

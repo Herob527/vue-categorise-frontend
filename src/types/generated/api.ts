06971702b2ab7afe314b596340a1ca66749d46f6
Herob527 <szymon.wrzos@onet.pl>
2024-05-01 00:51:30 +0200

    Regenerated types

--- src/types/generated/api.ts
+++ src/types/generated/api.ts
@@ -555,6 +555,44 @@ export const BindingsApiAxiosParamCreator = function (configuration?: Configurat
     return {
         /**
          * 
+         * @summary Binding Category Update
+         * @param {string} bindingId 
+         * @param {string} categoryId 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        bindingCategoryUpdateBindingsBindingIdCategoryAssignCategoryIdPut: async (bindingId: string, categoryId: string, options: RawAxiosRequestConfig = {}): Promise<RequestArgs> => {
+            // verify required parameter 'bindingId' is not null or undefined
+            assertParamExists('bindingCategoryUpdateBindingsBindingIdCategoryAssignCategoryIdPut', 'bindingId', bindingId)
+            // verify required parameter 'categoryId' is not null or undefined
+            assertParamExists('bindingCategoryUpdateBindingsBindingIdCategoryAssignCategoryIdPut', 'categoryId', categoryId)
+            const localVarPath = `/bindings/{binding_id}/category_assign/{category_id}`
+                .replace(`{${"binding_id"}}`, encodeURIComponent(String(bindingId)))
+                .replace(`{${"category_id"}}`, encodeURIComponent(String(categoryId)));
+            // use dummy base URL string because the URL constructor only accepts absolute URLs.
+            const localVarUrlObj = new URL(localVarPath, DUMMY_BASE_URL);
+            let baseOptions;
+            if (configuration) {
+                baseOptions = configuration.baseOptions;
+            }
+
+            const localVarRequestOptions = { method: 'PUT', ...baseOptions, ...options};
+            const localVarHeaderParameter = {} as any;
+            const localVarQueryParameter = {} as any;
+
+
+    
+            setSearchParams(localVarUrlObj, localVarQueryParameter);
+            let headersFromBaseOptions = baseOptions && baseOptions.headers ? baseOptions.headers : {};
+            localVarRequestOptions.headers = {...localVarHeaderParameter, ...headersFromBaseOptions, ...options.headers};
+
+            return {
+                url: toPathString(localVarUrlObj),
+                options: localVarRequestOptions,
+            };
+        },
+        /**
+         * 
          * @summary Create Binding
          * @param {File} audio 
          * @param {string} [category] 
@@ -784,6 +822,20 @@ export const BindingsApiFp = function(configuration?: Configuration) {
     return {
         /**
          * 
+         * @summary Binding Category Update
+         * @param {string} bindingId 
+         * @param {string} categoryId 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        async bindingCategoryUpdateBindingsBindingIdCategoryAssignCategoryIdPut(bindingId: string, categoryId: string, options?: RawAxiosRequestConfig): Promise<(axios?: AxiosInstance, basePath?: string) => AxiosPromise<any>> {
+            const localVarAxiosArgs = await localVarAxiosParamCreator.bindingCategoryUpdateBindingsBindingIdCategoryAssignCategoryIdPut(bindingId, categoryId, options);
+            const localVarOperationServerIndex = configuration?.serverIndex ?? 0;
+            const localVarOperationServerBasePath = operationServerMap['BindingsApi.bindingCategoryUpdateBindingsBindingIdCategoryAssignCategoryIdPut']?.[localVarOperationServerIndex]?.url;
+            return (axios, basePath) => createRequestFunction(localVarAxiosArgs, globalAxios, BASE_PATH, configuration)(axios, localVarOperationServerBasePath || basePath);
+        },
+        /**
+         * 
          * @summary Create Binding
          * @param {File} audio 
          * @param {string} [category] 
@@ -873,6 +925,17 @@ export const BindingsApiFactory = function (configuration?: Configuration, baseP
     return {
         /**
          * 
+         * @summary Binding Category Update
+         * @param {string} bindingId 
+         * @param {string} categoryId 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        bindingCategoryUpdateBindingsBindingIdCategoryAssignCategoryIdPut(bindingId: string, categoryId: string, options?: any): AxiosPromise<any> {
+            return localVarFp.bindingCategoryUpdateBindingsBindingIdCategoryAssignCategoryIdPut(bindingId, categoryId, options).then((request) => request(axios, basePath));
+        },
+        /**
+         * 
          * @summary Create Binding
          * @param {File} audio 
          * @param {string} [category] 
@@ -942,6 +1005,19 @@ export const BindingsApiFactory = function (configuration?: Configuration, baseP
  * @extends {BaseAPI}
  */
 export class BindingsApi extends BaseAPI {
+    /**
+     * 
+     * @summary Binding Category Update
+     * @param {string} bindingId 
+     * @param {string} categoryId 
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     * @memberof BindingsApi
+     */
+    public bindingCategoryUpdateBindingsBindingIdCategoryAssignCategoryIdPut(bindingId: string, categoryId: string, options?: RawAxiosRequestConfig) {
+        return BindingsApiFp(this.configuration).bindingCategoryUpdateBindingsBindingIdCategoryAssignCategoryIdPut(bindingId, categoryId, options).then((request) => request(this.axios, this.basePath));
+    }
+
     /**
      * 
      * @summary Create Binding

dfd67e3fcde976a1f7be6f014e7e7b0950773ac9
Herob527 <szymon.wrzos@onet.pl>
2024-05-05 22:04:58 +0200

    Extracted OptionPanelContainer into ModalComponent (not yet migrated)

---
+++ src/components/ModalComponent.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import ActionButton from '@/components/ActionButton.vue';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+
+defineEmits(['close']);
+</script>
+<template>
+  <div
+    class="fixed top-0 left-0 z-10 w-full h-full bg-black bg-opacity-50"
+    @click="$emit('close')"
+  ></div>
+  <div
+    class="flex fixed top-1/2 left-1/2 z-10 flex-col p-2 text-white -translate-x-1/2 -translate-y-1/2 bg-primary-500"
+  >
+    <div>
+      <ActionButton
+        :onClick="() => $emit('close')"
+        class-name="bg-primary-500 border-2 border-primary-600 hover:bg-primary-600 relative p-4 text-white rounded-xl"
+      >
+        <font-awesome-icon
+          :icon="faXmark"
+          class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+        />
+      </ActionButton>
+    </div>
+    <slot />
+  </div>
+</template>

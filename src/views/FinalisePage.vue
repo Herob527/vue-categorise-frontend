e0edb7f38fb967b0ee1bd04e719ff2a3f1d808ed
Herob527 <szymon.wrzos@onet.pl>
2024-05-05 16:29:48 +0200

    Added basics for finalization

--- src/views/FinalisePage.vue
+++ src/views/FinalisePage.vue
@@ -1,5 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ActionButton from '@/components/ActionButton.vue';
+import { useRouter } from 'vue-router';
+
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+
+const router = useRouter();
+
+const handleClick = () => {
+  router.replace({ name: 'home' });
+};
+</script>
 
 <template>
-  <p>Welcome to finalise</p>
+  <nav class="container my-4 mx-auto">
+    <ActionButton
+      :onClick="handleClick"
+      class-name="bg-blue-500 text-white px-4 py-2 rounded-xl"
+    >
+      <font-awesome-icon :icon="faArrowLeft" />
+      Go home
+    </ActionButton>
+  </nav>
+  <main>
+    <p>Test</p>
+  </main>
 </template>

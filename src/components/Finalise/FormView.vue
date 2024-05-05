4be4804dbb740d7ea27f83a4971c164f8d3f1ad7
Herob527 <szymon.wrzos@onet.pl>
2024-05-05 22:05:02 +0200

    Added help

--- src/components/Finalise/FormView.vue
+++ src/components/Finalise/FormView.vue
@@ -2,6 +2,9 @@
 import { useFinaliseStore } from '@/stores/finaliseStore';
 import type { MutationStatus } from '@tanstack/vue-query';
 import { ref } from 'vue';
+import { faQuestion } from '@fortawesome/free-solid-svg-icons';
+import ActionButton from '../ActionButton.vue';
+import ModalComponent from '../ModalComponent.vue';
 
 defineProps<{ submitStatus: MutationStatus }>();
 
@@ -21,6 +24,7 @@ enum ERRORS {
 const values = useFinaliseStore();
 
 const errors = ref<ERRORS[]>([]);
+const isHelpOpen = ref(false);
 
 const validate = () => {
   errors.value = [];
@@ -49,7 +53,114 @@ defineEmits(['submit']);
 
 <template>
   <section class="flex flex-col gap-2 p-2 border-2 border-primary-500">
-    <h2 class="text-2xl font-bold">Finalise options</h2>
+    <header class="flex flex-row gap-2 items-center">
+      <h2 class="text-2xl font-bold">Finalise options</h2>
+      <ActionButton
+        @click="isHelpOpen = !isHelpOpen"
+        class-name="bg-blue-500 text-white p-4 rounded-xl relative"
+      >
+        <font-awesome-icon
+          :icon="faQuestion"
+          class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+        />
+      </ActionButton>
+    </header>
+    <ModalComponent v-if="isHelpOpen" @close="isHelpOpen = false">
+      <div class="flex flex-col gap-2 mt-2">
+        <h2 class="text-2xl font-bold">Help</h2>
+        <div class="p-2 bg-white text-primary-500">
+          <h3 class="text-xl font-bold">Omit empty texts</h3>
+
+          <div class="pl-2">
+            <div class="flex flex-row gap-2">
+              <input type="checkbox" checked readonly />
+              <span
+                >Empty transcripts will be <b>omitted</b> in generated
+                transcript</span
+              >
+            </div>
+            <div class="flex flex-row gap-2">
+              <input type="checkbox" readonly />
+              <span
+                >Empty transcripts will be <b>incluced</b> in generated
+                transcript</span
+              >
+            </div>
+          </div>
+        </div>
+        <div class="p-2 bg-white text-primary-500">
+          <h3 class="text-xl font-bold">Divide by category</h3>
+          <div class="pl-2">
+            <div class="flex flex-row gap-2">
+              <input type="checkbox" checked readonly />
+              <span
+                >Audio files will <b>be divided</b> by category (in different
+                directories)</span
+              >
+            </div>
+            <div class="flex flex-row gap-2">
+              <input type="checkbox" readonly />
+              <span>
+                <span
+                  >Audio files will <b>share</b> one directory regardless of
+                  category</span
+                >
+              </span>
+            </div>
+            <span
+              >Note: In both case, categories will be assigned in
+              transcript</span
+            >
+          </div>
+        </div>
+        <div class="p-2 bg-white text-primary-500">
+          <h3 class="text-xl font-bold">Export transcript</h3>
+          <div class="pl-2">
+            <div class="flex flex-row gap-2">
+              <input type="checkbox" checked readonly />
+              <span>Transcript <b>will</b> be generated</span>
+            </div>
+            <div class="flex flex-row gap-2">
+              <input type="checkbox" readonly />
+              <span>
+                <span>Transcript <b>won't</b> be generated</span>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="p-2 bg-white text-primary-500">
+          <h3 class="text-xl font-bold">Uncategorized name</h3>
+          <p class="pl-2">
+            Name of category for uncatergized audio files in output
+          </p>
+          <p class="pl-2">It's required</p>
+        </div>
+
+        <div class="p-2 bg-white text-primary-500">
+          <h3 class="text-xl font-bold">Line format</h3>
+          <div class="flex flex-col pl-2 class">
+            <span> Format of line in transcript </span>
+            <span
+              >Text between curly brackets is used for substitution of values
+              from database</span
+            >
+            <span> Currently supported values:</span>
+            <ul class="pl-6 list-disc">
+              <li><b>file</b> - file name of audio file</li>
+              <li><b>text</b> - transcript of audio</li>
+              <li><b>category</b> - category of audio</li>
+              <li><b>category_index</b> - numeric category index of audio</li>
+              <li><b>duration</b> - length of audio in seconds</li>
+            </ul>
+            <span class="py-2"
+              ><b>Example</b>: "{file}|{text}" -> "my_file.mp3|my text"</span
+            >
+            <span>Currently you can use only supported values.</span>
+            <span>Ones outside will throw <b>validation error</b></span>
+          </div>
+        </div>
+      </div>
+    </ModalComponent>
     <div class="flex flex-row gap-2">
       <input
         type="checkbox"
@@ -111,7 +222,7 @@ defineEmits(['submit']);
     <div class="flex flex-row gap-2">
       <button
         type="button"
-        class="flex-1 p-2 text-white bg-blue-500 disabled:bg-gray-300"
+        class="p-2 text-white bg-blue-500 disabled:bg-gray-300"
         :disabled="submitStatus === 'pending'"
         @click="!(submitStatus === 'pending') && validate() && $emit('submit')"
       >

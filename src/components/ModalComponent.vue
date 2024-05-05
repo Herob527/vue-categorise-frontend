c0c8971a074805ee7fae5b0f0fc8e25debfb6362
Herob527 <szymon.wrzos@onet.pl>
2024-05-05 23:08:41 +0200

    Moved title before button and made button to the right

--- src/components/ModalComponent.vue
+++ src/components/ModalComponent.vue
@@ -13,8 +13,9 @@ defineEmits(['close']);
     class="flex overflow-auto fixed top-1/2 left-1/2 z-10 flex-col p-2 pt-0 max-h-screen text-white -translate-x-1/2 -translate-y-1/2 bg-primary-500"
   >
     <div
-      class="flex sticky top-0 flex-row gap-2 items-center pb-2 mt-2 bg-primary-500"
+      class="flex sticky top-0 flex-row gap-2 justify-between items-center pb-2 mt-2 w-full bg-primary-500"
     >
+      <h2 class="text-2xl font-bold">{{ title || '' }}</h2>
       <ActionButton
         :onClick="() => $emit('close')"
         class-name="bg-primary-500 border-2 border-primary-600 hover:bg-primary-600 relative p-4 text-white rounded-xl"
@@ -24,8 +25,6 @@ defineEmits(['close']);
           class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
         />
       </ActionButton>
-
-      <h2 class="text-2xl font-bold">{{ title || '' }}</h2>
     </div>
     <slot />
   </div>

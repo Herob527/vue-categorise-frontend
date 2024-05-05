5c97db69b0527b2afeb8c41e452d245b9aaf3480
Herob527 <szymon.wrzos@onet.pl>
2024-05-05 23:07:24 +0200

    Added title prop and moved it to be close to closing button

--- src/components/ModalComponent.vue
+++ src/components/ModalComponent.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import ActionButton from '@/components/ActionButton.vue';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
-
+defineProps<{ title?: string }>();
 defineEmits(['close']);
 </script>
 <template>
@@ -10,9 +10,11 @@ defineEmits(['close']);
     @click="$emit('close')"
   ></div>
   <div
-    class="flex fixed top-1/2 left-1/2 z-10 flex-col p-2 max-h-screen text-white -translate-x-1/2 -translate-y-1/2 bg-primary-500"
+    class="flex overflow-auto fixed top-1/2 left-1/2 z-10 flex-col p-2 pt-0 max-h-screen text-white -translate-x-1/2 -translate-y-1/2 bg-primary-500"
   >
-    <div>
+    <div
+      class="flex sticky top-0 flex-row gap-2 items-center pb-2 mt-2 bg-primary-500"
+    >
       <ActionButton
         :onClick="() => $emit('close')"
         class-name="bg-primary-500 border-2 border-primary-600 hover:bg-primary-600 relative p-4 text-white rounded-xl"
@@ -22,6 +24,8 @@ defineEmits(['close']);
           class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
         />
       </ActionButton>
+
+      <h2 class="text-2xl font-bold">{{ title || '' }}</h2>
     </div>
     <slot />
   </div>

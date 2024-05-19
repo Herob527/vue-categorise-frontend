e7687bd9e1bbd31f836f14c329db144a526d90f5
Herob527 <szymon.wrzos@onet.pl>
2024-05-19 18:23:16 +0200

    Formatting and working on fixing being out of sync with options

--- src/components/Finalise/Preview/DirectoriesPreview.vue
+++ src/components/Finalise/Preview/DirectoriesPreview.vue
@@ -2,35 +2,45 @@
 import { useFinalisePreviewStore } from '@/stores/previewStore';
 import DirectoryItem from './DirectoriesPreview/DirectoryItem.vue';
 import type { DataProp, FileShape } from '@/types/shared';
+import { computed } from 'vue';
 
-const { fakeCategories, fakeData } = useFinalisePreviewStore();
+const { filteredData: fakeData } = useFinalisePreviewStore();
 
 const getDataForCategory = (category: string) =>
   fakeData
     .filter((d) => d.category === category)
     .map(
       (entry) =>
-        ({ fileName: entry.fileName, isDirectory: false }) satisfies FileShape,
+        ({
+          fileName: entry.fileName,
+          isDirectory: false,
+        }) satisfies FileShape,
     );
 
-const getDataProps = (category: string) =>
-  [
-    {
-      dirName: 'wavs',
-      files: getDataForCategory(category),
-      isDirectory: true,
-    },
-    {
-      fileName: 'transcript.txt',
-      isDirectory: false,
-    },
-  ] satisfies DataProp[];
+const getDataProps = computed(
+  () => (category: string) =>
+    [
+      {
+        dirName: 'wavs',
+        files: getDataForCategory(category),
+        isDirectory: true,
+      },
+      {
+        fileName: 'transcript.txt',
+        isDirectory: false,
+      },
+    ] satisfies DataProp[],
+);
+
+const categories = computed(
+  () => new Set(fakeData.map((entry) => entry.category)),
+);
 </script>
 
 <template>
-  <div class="flex flex-col">
+  <div class="flex flex-col flex-wrap gap-4 max-h-[500px]">
     <DirectoryItem
-      v-for="category in fakeCategories"
+      v-for="category in categories"
       :key="category"
       :name="category"
       :data="getDataProps(category)"

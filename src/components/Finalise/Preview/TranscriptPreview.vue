ebc3762310c17e8f0567f708ca49470deebc9a3a
Herob527 <szymon.wrzos@onet.pl>
2024-05-19 14:19:56 +0200

    Moved generating fake data to preview store

--- src/components/Finalise/Preview/TranscriptPreview.vue
+++ src/components/Finalise/Preview/TranscriptPreview.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-import { computed, onMounted, ref } from 'vue';
-import { faker } from '@faker-js/faker';
+import { computed } from 'vue';
 import { useFinaliseStore } from '@/stores/finaliseStore';
-import { FilterFactory } from '@/utils/FilterFactory';
-import { ProcessingPipeline } from '@/utils/ProcessingPipeline';
+import { useFinalisePreviewStore } from '@/stores/previewStore';
 
 const store = useFinaliseStore();
+const previewStore = useFinalisePreviewStore();
 
 // Define the type for the data structure
 interface ExampleDataItem {
@@ -16,71 +15,7 @@ interface ExampleDataItem {
   duration: number;
 }
 
-const categories = ref<string[]>([]);
-const extensions = ['wav', 'mp3'] as const;
-
 const TRANSCRIPT_ENTRIES = 15;
-
-const exampleData = ref<ExampleDataItem[]>([]);
-
-const filteredData = computed(() => {
-  const filter = new FilterFactory(exampleData.value);
-  const filteredEntries = filter
-    .addFilter((item) => item.text.trim() !== '', store.omit_empty)
-    .filter();
-  const processing = new ProcessingPipeline(filteredEntries);
-  const processedData = processing
-    .addStage(
-      (value) => ({
-        ...value,
-        category: value.category.toLowerCase(),
-      }),
-      store.category_to_lower,
-    )
-    .addStage(
-      (value) => ({
-        ...value,
-        category: value.category.replace(/\s+/, store.category_space_replacer),
-      }),
-      !!store.category_space_replacer,
-    )
-    .process();
-
-  return processedData;
-});
-
-onMounted(() => {
-  categories.value = faker.lorem
-    .words(5)
-    .split(' ')
-    .map((entry) => {
-      const randomIndex = Math.floor(Math.random() * TRANSCRIPT_ENTRIES);
-      const splitVal = entry.split('');
-      if (randomIndex > TRANSCRIPT_ENTRIES / 2) {
-        return faker.lorem.words(2);
-      }
-      if (randomIndex < TRANSCRIPT_ENTRIES / 2) {
-        return [splitVal[0].toUpperCase(), ...splitVal.slice(1)].join('');
-      } else {
-        return splitVal.join('');
-      }
-    });
-
-  exampleData.value = Array.from({ length: TRANSCRIPT_ENTRIES }, () => {
-    const categoryIndex = Math.floor(Math.random() * categories.value.length);
-    const category = categories.value[categoryIndex];
-    const fileName = faker.system.fileName({ extensionCount: 0 });
-    const extension = faker.helpers.arrayElement(extensions);
-    const text = faker.lorem.sentences({ min: 0, max: 2 });
-    return {
-      fileName: `${fileName}.${extension}`,
-      text: text.length > 10 ? `${text.substring(0, 10)}...` : text,
-      categoryIndex,
-      category,
-      duration: faker.number.float({ max: 10, min: 0, fractionDigits: 2 }),
-    };
-  });
-});
 
 const processLine = computed(() => (param: ExampleDataItem) => {
   const { fileName, text, categoryIndex, category, duration } = param;
@@ -100,7 +35,7 @@ const processLine = computed(() => (param: ExampleDataItem) => {
     <h2 class="px-2 my-2 text-2xl font-bold">Transcript preview</h2>
     <div class="flex flex-col">
       <div
-        v-for="[index, example] in Object.entries(filteredData)"
+        v-for="[index, example] in Object.entries(previewStore.filteredData)"
         :key="example.fileName"
         class="flex entry-container"
       >
---
+++ src/stores/previewStore.ts
@@ -0,0 +1,88 @@
+import { acceptHMRUpdate, defineStore } from 'pinia';
+import { useFinaliseStore } from './finaliseStore';
+import { computed } from 'vue';
+import { FilterFactory } from '@/utils/FilterFactory';
+import { ProcessingPipeline } from '@/utils/ProcessingPipeline';
+import { faker } from '@faker-js/faker';
+
+const TRANSCRIPT_ENTRIES = 15;
+
+const EXTENSIONS = ['wav', 'mp3'] as const;
+
+export const useFinalisePreviewStore = defineStore('preview-finalise', () => {
+  const store = useFinaliseStore();
+
+  const fakeData = computed(() => {
+    const categories = fakeCategories.value;
+    return Array.from({ length: TRANSCRIPT_ENTRIES }, () => {
+      const categoryIndex = Math.floor(Math.random() * categories.length);
+      const category = categories[categoryIndex];
+      const fileName = faker.system.fileName({ extensionCount: 0 });
+      const extension = faker.helpers.arrayElement(EXTENSIONS);
+      const text = faker.lorem.sentences({ min: 0, max: 2 });
+      return {
+        fileName: `${fileName}.${extension}`,
+        text: text.length > 10 ? `${text.substring(0, 10)}...` : text,
+        categoryIndex,
+        category,
+        duration: faker.number.float({ max: 10, min: 0, fractionDigits: 2 }),
+      };
+    });
+  });
+
+  const fakeCategories = computed(() => {
+    return faker.lorem
+      .words(5)
+      .split(' ')
+      .map((entry) => {
+        const randomIndex = Math.floor(Math.random() * TRANSCRIPT_ENTRIES);
+        const splitVal = entry.split('');
+        if (randomIndex > TRANSCRIPT_ENTRIES / 2) {
+          return faker.lorem.words(2);
+        }
+        if (randomIndex < TRANSCRIPT_ENTRIES / 2) {
+          return [splitVal[0].toUpperCase(), ...splitVal.slice(1)].join('');
+        } else {
+          return splitVal.join('');
+        }
+      });
+  });
+
+  const filteredData = computed(() => {
+    const data = fakeData.value;
+    const filter = new FilterFactory(data);
+    const filteredEntries = filter
+      .addFilter((item) => item.text.trim() !== '', store.omit_empty)
+      .filter();
+    const processing = new ProcessingPipeline(filteredEntries);
+    const processedData = processing
+      .addStage(
+        (value) => ({
+          ...value,
+          category: value.category.toLowerCase(),
+        }),
+        store.category_to_lower,
+      )
+      .addStage(
+        (value) => ({
+          ...value,
+          category: value.category.replace(
+            /\s+/,
+            store.category_space_replacer,
+          ),
+        }),
+        !!store.category_space_replacer,
+      )
+      .process();
+
+    return processedData;
+  });
+  return { fakeData, fakeCategories, filteredData };
+});
+
+// make sure to pass the right store definition, `useAuth` in this case.
+if (import.meta.hot) {
+  import.meta.hot.accept(
+    acceptHMRUpdate(useFinalisePreviewStore, import.meta.hot),
+  );
+}

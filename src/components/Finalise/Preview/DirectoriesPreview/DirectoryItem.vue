5cb1ddf700900aa5a1bce502b5c2db828335dbed
Herob527 <szymon.wrzos@onet.pl>
2024-05-19 19:57:38 +0200

    Working on styling

--- src/components/Finalise/Preview/DirectoriesPreview/DirectoryItem.vue
+++ src/components/Finalise/Preview/DirectoriesPreview/DirectoryItem.vue
@@ -9,23 +9,25 @@ defineProps<{ name: string; data: DataProp[]; depth?: number }>();
 const isOpen = ref(true);
 </script>
 <template>
-  <div :class="`flex flex-col ${depth ? 'ml-2' : ''}`">
+  <div
+    :class="`flex flex-col ${depth ? 'ml-2' : 'border-black border-2 p-2 rounded-xl'}`"
+  >
     <button
       type="button"
       @click="isOpen = !isOpen"
-      class="flex gap-2 items-center"
+      class="flex gap-2 items-center py-1 px-2 rounded-xl hover:text-white hover:bg-primary-500"
     >
       <font-awesome-icon width="16" :icon="isOpen ? faFolderOpen : faFolder" />
       <span>{{ name }}</span>
     </button>
-    <div v-if="isOpen" class="ml-1.5">
+    <div v-if="isOpen" class="flex flex-col flex-wrap ml-1.5">
       <div
         v-for="entry in data"
         :key="entry.isDirectory ? entry.dirName : entry.fileName"
-        class="flex relative gap-2 border-l-2 border-black"
+        class="flex relative gap-2"
       >
         <template v-if="entry.isDirectory === false">
-          <div class="flex flex-row flex-1 gap-2 items-center ml-2">
+          <div class="flex flex-row flex-1 gap-2 items-center py-1 px-2 ml-2">
             <FileIcon :file-name="entry.fileName" />
             <span>{{ entry.fileName }} </span>
           </div>

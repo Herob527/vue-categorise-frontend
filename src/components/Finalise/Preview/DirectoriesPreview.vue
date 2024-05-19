e651ec67c2403528a3109139e4724c9f146946a6
Herob527 <szymon.wrzos@onet.pl>
2024-05-19 18:59:38 +0200

    Added support for option for preview without divinding

--- src/components/Finalise/Preview/DirectoriesPreview.vue
+++ src/components/Finalise/Preview/DirectoriesPreview.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 import { useFinalisePreviewStore } from '@/stores/previewStore';
 import DirectoryItem from './DirectoriesPreview/DirectoryItem.vue';
-import type { DataProp, FileShape } from '@/types/shared';
+import type { DataProp, DirectoryShape, FileShape } from '@/types/shared';
 import { computed } from 'vue';
 import { storeToRefs } from 'pinia';
 import { useFinaliseStore } from '@/stores/finaliseStore';
 
 const { filteredData } = storeToRefs(useFinalisePreviewStore());
-const { export_transcript } = storeToRefs(useFinaliseStore());
+const { export_transcript, divide_by_category } =
+  storeToRefs(useFinaliseStore());
 
 const getDataForCategory = (category: string) =>
   filteredData.value
@@ -21,13 +22,24 @@ const getDataForCategory = (category: string) =>
     );
 
 const getDataProps = computed(
-  () => (category: string) =>
+  () => (category?: string) =>
     [
-      {
-        dirName: 'wavs',
-        files: getDataForCategory(category),
-        isDirectory: true,
-      },
+      ...[
+        {
+          dirName: 'wavs',
+          files:
+            divide_by_category.value && category
+              ? getDataForCategory(category)
+              : filteredData.value.map(
+                  (entry) =>
+                    ({
+                      fileName: entry.fileName,
+                      isDirectory: false,
+                    }) satisfies FileShape,
+                ),
+          isDirectory: true,
+        } satisfies DirectoryShape,
+      ],
 
       ...(export_transcript.value
         ? [
@@ -47,11 +59,16 @@ const categories = computed(() => [
 
 <template>
   <div class="flex flex-col flex-wrap gap-4 max-h-[500px]">
-    <DirectoryItem
-      v-for="category in categories"
-      :key="category"
-      :name="category"
-      :data="getDataProps(category)"
-    />
+    <template v-if="divide_by_category">
+      <DirectoryItem
+        v-for="category in categories"
+        :key="category"
+        :name="category"
+        :data="getDataProps(category)"
+      />
+    </template>
+    <template v-else>
+      <DirectoryItem :name="'(root)'" :data="getDataProps()" />
+    </template>
   </div>
 </template>

ae436252fb3c694d8e6a1751ccde264ebfb75192
Herob527 <szymon.wrzos@onet.pl>
2024-05-23 23:08:43 +0200

    Created preview container for real data

---
+++ src/components/Finalise/RealDataPreview/PreviewContainer.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { useFinaliseRealPreviewStore } from '@/stores/finalisationDataStore';
+import DirectoryItem from '../Preview/DirectoriesPreview/DirectoryItem.vue';
+
+const store = useFinaliseRealPreviewStore();
+</script>
+<template>
+  <section
+    class="container flex overflow-scroll flex-col rounded-xl border-2 h-min max-h-[500px] border-primary-500"
+  >
+    <h2 class="px-2 my-2 text-2xl font-bold">Folder preview (Real)</h2>
+    <div class="flex flex-col flex-wrap gap-3 px-2 pb-2">
+      <template v-if="store.processedData">
+        <DirectoryItem
+          v-for="category in store.processedData"
+          :key="category.dirName"
+          :name="category.dirName"
+          :data="category.files"
+        />
+      </template>
+      <template v-else>
+        <p>Data not there yet</p>
+      </template>
+    </div>
+  </section>
+</template>

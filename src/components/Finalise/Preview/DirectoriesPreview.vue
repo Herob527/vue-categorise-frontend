3ccc456d436a1aaea1faaf57fb884e3f0bcdd577
Herob527 <szymon.wrzos@onet.pl>
2024-05-23 23:08:31 +0200

    Stated explicitly that preview is simulated

--- src/components/Finalise/Preview/DirectoriesPreview.vue
+++ src/components/Finalise/Preview/DirectoriesPreview.vue
@@ -61,7 +61,7 @@ const categories = computed(() => [
   <section
     class="container flex overflow-scroll flex-col rounded-xl border-2 h-min max-h-[500px] border-primary-500"
   >
-    <h2 class="px-2 my-2 text-2xl font-bold">Folder preview</h2>
+    <h2 class="px-2 my-2 text-2xl font-bold">Folder preview (simulated)</h2>
     <div class="flex flex-col flex-wrap gap-3 px-2 pb-2">
       <template v-if="divide_by_category">
         <DirectoryItem

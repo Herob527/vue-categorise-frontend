39a77023efb329eb262d79d6b0d1a78004260a05
Herob527 <szymon.wrzos@onet.pl>
2024-05-23 23:08:16 +0200

    Removed redundant types

--- src/actions/finalise.ts
+++ src/actions/finalise.ts
@@ -1,8 +1,4 @@
-import {
-  FinaliseApi,
-  type FileModel,
-  type FinaliseConfigModel,
-} from '@/types/generated';
+import { FinaliseApi, type FinaliseConfigModel } from '@/types/generated';
 
 const finaliseApi = new FinaliseApi(
   {
@@ -11,17 +7,8 @@ const finaliseApi = new FinaliseApi(
   'http://localhost:8000',
 );
 
-type DirectoryShape = {
-  dir_name: string;
-  files: (FileModel | DirectoryShape)[];
-  is_dir: true;
-};
-
-export type DataProp = FileModel | DirectoryShape;
-
 export const post = async (params: FinaliseConfigModel = {}) => {
   const { data } = await finaliseApi.finaliseFinalisePost(params);
 
-  // Necessary for proper recursive typing
-  return data as unknown as DataProp;
+  return data;
 };

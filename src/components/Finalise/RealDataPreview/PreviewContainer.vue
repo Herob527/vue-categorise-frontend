12b3e764c88ddf093ecfc6e31be4a2773ecaf3cb
Herob527 <szymon.wrzos@onet.pl>
2024-05-23 23:36:49 +0200

    Added handling case if current entry in file

--- src/components/Finalise/RealDataPreview/PreviewContainer.vue
+++ src/components/Finalise/RealDataPreview/PreviewContainer.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useFinaliseRealPreviewStore } from '@/stores/finalisationDataStore';
 import DirectoryItem from '../Preview/DirectoriesPreview/DirectoryItem.vue';
+import FileIcon from '../Preview/DirectoriesPreview/FileIcon.vue';
 
 const store = useFinaliseRealPreviewStore();
 </script>
@@ -12,7 +13,7 @@ const store = useFinaliseRealPreviewStore();
     <div class="flex flex-col flex-wrap gap-3 px-2 pb-2">
       <template v-if="store.processedData">
         <template
-          v-for="category in store.processedData"
+          v-for="[index, category] in Object.entries(store.processedData.files)"
           :key="category.isDirectory ? category.dirName : category.fileName"
         >
           <DirectoryItem
@@ -20,6 +21,17 @@ const store = useFinaliseRealPreviewStore();
             :name="category.dirName"
             :data="category.files"
           />
+
+          <div
+            class="flex flex-row flex-1 gap-2 items-center py-3 px-4 rounded-xl border-2 border-primary-500"
+            v-else
+          >
+            <FileIcon
+              :file-name="category.fileName"
+              :is-even-index="Number(index) % 2 === 0"
+            />
+            <span>{{ category.fileName }} </span>
+          </div>
         </template>
       </template>
       <template v-else>

1e1752561811a778a32a5bc2c1abf889110ff6c6
Herob527 <szymon.wrzos@onet.pl>
2024-06-09 22:25:56 +0200

    Updates and removed plugin jsx for vue

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,11 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
 import svgLoader from 'vite-svg-loader';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), svgLoader()],
+  plugins: [vue(), svgLoader()],
   server: {
     open: '/',
   },

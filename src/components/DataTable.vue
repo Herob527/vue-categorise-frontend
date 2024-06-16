d26c3d3892b51f887bb2c9fdcf6d6d4ef2651701
Herob527 <szymon.wrzos@onet.pl>
2024-06-16 23:01:20 +0200

    Renaming and adjustments of data table + working on loading view

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -7,21 +7,22 @@ const props = defineProps<{
   title?: string;
   itemKeys?: string[] | readonly string[];
   itemsCount?: number;
+  isLoading?: boolean;
 }>();
 
 defineEmits<{
   'submit:page': [value: number];
 }>();
 
-const page = ref<number | null>(null);
-const selectedPage = ref<number | null>(null);
+const selectedPage = ref<number | null>(0);
+const pickedJumpPage = ref<number | null>(null);
 const pageSize = 20;
 
 const splitToPages = (amountOfEntries: number): (number | 'dot')[] => {
   const pages = Array.from(Array(amountOfEntries).keys());
   const firstPage = 0;
   const lastPage = amountOfEntries - 1;
-  const usedPage = page.value || 0;
+  const usedPage = selectedPage.value || 0;
   const lowerBound = usedPage - 1 > 0 ? usedPage - 1 : 0;
   const upperBound = usedPage + 2;
 
@@ -47,38 +48,17 @@ const pages = computed(() =>
 console.log(pages.value);
 
 const isValid = computed(() => {
-  if (!selectedPage.value) return true;
+  if (!pickedJumpPage.value) return true;
   return (
-    selectedPage.value > 0 && selectedPage.value < (props.itemsCount || 0) + 1
+    pickedJumpPage.value > 0 &&
+    pickedJumpPage.value < (props.itemsCount || 0) + 1
   );
 });
 </script>
 <template>
-  <div
-    v-if="props.data.length === 0"
-    class="flex overflow-hidden flex-col rounded-xl border-2 border-primary-500"
-  >
-    <p class="p-2 text-2xl font-bold text-white uppercase bg-primary-600">
-      {{ props.title }}
-    </p>
-    <div
-      class="flex flex-row items-center py-2 text-xl font-bold text-center text-white uppercase bg-primary-500"
-    >
-      <span class="flex-1" v-for="item in itemKeys" :key="item">{{
-        item
-      }}</span>
-    </div>
-    <slot name="fallback" v-if="props.data.length === 0" />
-  </div>
-
-  <div
-    v-for="[index, item] in Object.entries(data).slice(
-      (page || 0) * pageSize,
-      ((page || 0) + 1) * pageSize,
-    )"
-    :key="index"
-  >
-    <div v-if="Number(index) === 0" class="flex flex-col">
+  <div>
+    <div class="flex flex-col">
+      <!-- Heading -->
       <p class="p-2 text-2xl font-bold text-white uppercase bg-primary-600">
         {{ props.title }}
       </p>
@@ -90,55 +70,71 @@ const isValid = computed(() => {
         }}</span>
       </div>
     </div>
-    <slot name="item" :index="Number(index)" :entry="item" />
-  </div>
-  <div class="inline-flex flex-row gap-2">
-    <template v-for="entryPage in pages" :key="entryPage">
-      <button
-        v-if="entryPage !== 'dot'"
-        type="button"
-        class="relative w-10 h-10 bg-primary-200"
-        @click="
-          () => {
-            console.log(entryPage);
-            page = entryPage;
-            $emit('submit:page', entryPage);
-          }
-        "
-      >
-        <span>{{ entryPage + 1 }}</span>
-      </button>
-      <div v-else class="flex flex-col gap-2 w-10">
-        <input
-          type="text"
-          inputmode="numeric"
-          class="w-10 h-10 text-center border-2 border-primary-500"
-          v-model="selectedPage"
-          placeholder="..."
-        />
+    <div
+      v-if="data.length === 0 && (isLoading ?? false) === false"
+      class="flex overflow-hidden flex-col border-2 border-primary-500"
+    >
+      <slot name="fallback" />
+    </div>
+
+    <div
+      v-for="[index, item] in Object.entries(data).slice(
+        (selectedPage || 0) * pageSize,
+        ((selectedPage || 0) + 1) * pageSize,
+      )"
+      :key="index"
+    >
+      <slot name="item" :index="Number(index)" :entry="item" />
+    </div>
+    <div class="inline-flex flex-row gap-2 mt-2">
+      <!-- Pagination -->
+      <template v-for="entryPage in pages" :key="entryPage">
         <button
+          v-if="entryPage !== 'dot'"
           type="button"
-          class="inline-flex justify-center items-center w-10 h-10 text-white border-2 bg-primary-600 border-primary-500 hover:bg-primary-500"
+          :class="`relative w-10 h-10 ${entryPage === selectedPage ? 'bg-primary-600 hover:bg-primary-700' : 'bg-primary-500 hover:bg-primary-600'} text-white`"
           @click="
             () => {
-              console.log(selectedPage, isValid);
-              if (!selectedPage) return;
-              if (!isValid) return;
-
-              $emit('submit:page', selectedPage);
-              page = selectedPage;
+              console.log(entryPage);
+              selectedPage = entryPage;
+              $emit('submit:page', entryPage);
             }
           "
         >
-          Go
+          <span>{{ entryPage + 1 }}</span>
         </button>
-        <span
-          v-if="!isValid"
-          class="absolute right-1/2 top-full text-center text-red-500 translate-x-1/2"
-        >
-          Allowed range: {{ 1 }} - {{ props?.itemsCount || 1 }}
-        </span>
-      </div>
-    </template>
+        <div v-else class="flex flex-col gap-2 w-10">
+          <input
+            type="text"
+            inputmode="numeric"
+            class="w-10 h-10 text-center border-2 border-primary-500"
+            v-model="pickedJumpPage"
+            placeholder="..."
+          />
+          <button
+            type="button"
+            class="inline-flex justify-center items-center w-10 h-10 text-white border-2 bg-primary-600 border-primary-500 hover:bg-primary-500"
+            @click="
+              () => {
+                console.log(pickedJumpPage, isValid);
+                if (!pickedJumpPage) return;
+                if (!isValid) return;
+
+                $emit('submit:page', pickedJumpPage);
+                selectedPage = pickedJumpPage;
+              }
+            "
+          >
+            Go
+          </button>
+          <span
+            v-if="!isValid"
+            class="absolute right-1/2 top-full text-center text-red-500 translate-x-1/2"
+          >
+            Allowed range: {{ 1 }} - {{ props?.itemsCount || 1 }}
+          </span>
+        </div>
+      </template>
+    </div>
   </div>
 </template>

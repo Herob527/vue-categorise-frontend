e36c1d14b5dc76913a5c6ef648aa2301ed33a75e
Herob527 <szymon.wrzos@onet.pl>
2024-06-23 14:13:40 +0200

    Readjusted splitToPages utils

--- src/utils/splitToPages.ts
+++ src/utils/splitToPages.ts
@@ -1,24 +1,50 @@
-
-export const splitToPages = ({amountOfEntries , selectedPage } = {amountOfEntries: 1, selectedPage: 0}): (number | 'dot')[] => {
-  const pages = Array.from(Array(amountOfEntries).keys());
-  const firstPage = 0;
-  const lastPage = amountOfEntries - 1;
-  const usedPage = selectedPage || 0;
-  const lowerBound = usedPage - 1 > 0 ? usedPage - 1 : 0;
-  const upperBound = usedPage + 2;
-
-  const inBetween = pages.slice(lowerBound, upperBound);
-  console.table([
-    ['lowerBound', lowerBound],
-    ['upperBound', upperBound],
-    ['usedPage', usedPage],
-  ]);
-  if (inBetween[0] === 0) {
-    return [0, 'dot', 1, 2, 3, 'dot', lastPage];
-  } else if (inBetween[inBetween.length - 1] === pages.length - 1) {
-    return [firstPage, 'dot', ...inBetween];
+export const splitToPages = (
+  { amountOfEntries = 1, selectedPage = 0, pageSize = 10 } = {
+    amountOfEntries: 1,
+    selectedPage: 0,
+    pageSize: 10,
+  },
+): (number | 'dot')[] => {
+  if (amountOfEntries <= pageSize) {
+    return [1];
   }
-  console.log('inBetween', inBetween);
+  const resultPages = [];
+  for (let i = 1; i <= Math.ceil(amountOfEntries / pageSize); i++) {
+    resultPages.push(i);
+  }
+  if (selectedPage <= 1) {
+    return [0, 1, 2, 'dot', resultPages.at(-1) as number];
+  }
+  if (selectedPage == 2) {
+    return [0, 1, 2, 3, 'dot', resultPages.at(-1) as number];
+  }
+  if (selectedPage == resultPages.length - 3) {
+    return [
+      0,
+      'dot',
+      ...[
+        ...(resultPages.slice(resultPages.length - 4, resultPages.length) ||
+          []),
+      ],
+    ];
+  }
 
-  return [firstPage, 'dot', ...inBetween, 'dot', lastPage] as const;
+  if (selectedPage >= resultPages.length - 2) {
+    return [
+      0,
+      'dot',
+      ...[
+        ...(resultPages.slice(resultPages.length - 3, resultPages.length) ||
+          []),
+      ],
+    ];
+  }
+
+  return [
+    0,
+    'dot',
+    ...(resultPages.slice(selectedPage - 2, selectedPage + 1) || []),
+    'dot',
+    resultPages.at(-1) as number,
+  ];
 };

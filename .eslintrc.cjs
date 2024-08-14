c3e4fe7325786abee9623fa38932dc7e546a6c15
Herob527 <szymon.wrzos@onet.pl>
2024-08-14 12:37:40 +0200

    Added overrides for cjs and formatting + typing

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,31 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
+/**
+ * @type {import('eslint').ESLint.ConfigData}
+ */
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
   ],
+  env: {
+    browser: true,
+    node: false,
+  },
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: 'latest',
+  },
+  overrides: [
+    {
+      files: '**/*.cjs',
+      env: {
+        node: true,
+        browser: false,
+      },
+    },
+  ],
+};

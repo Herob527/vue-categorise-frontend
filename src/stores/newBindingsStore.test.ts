5b70652ded4d5c139880e605bb9b07b64e404b5e
Herob527 <szymon.wrzos@onet.pl>
2024-11-11 17:49:55 +0100

    Added test for removing

--- src/stores/newBindingsStore.test.ts
+++ src/stores/newBindingsStore.test.ts
@@ -6,9 +6,11 @@ describe('New bindings Store', () => {
   beforeEach(() => {
     setActivePinia(createPinia());
   });
+
   test('Add new local file', () => {
     const store = useBindingsStoreV2();
     store.addFile({
+      id: 'test',
       duration: 5,
       file: new File([], 'Test'),
       status: Statuses.PENDING,
@@ -16,5 +18,20 @@ describe('New bindings Store', () => {
     });
     expect(store.getAll.length).toBe(1);
     expect(store.getFieldsByStatus(Statuses.PENDING, 0).length).toBe(1);
+  });
+  test('Remove file', () => {
+    const store = useBindingsStoreV2();
+    store.addFile({
+      id: 'test',
+      duration: 5,
+      file: new File([], 'Test'),
+      status: Statuses.PENDING,
+      page: 0,
+    });
+
+    store.removeFile(Statuses.PENDING, 'test');
+
+    expect(store.getAll.length).toBe(0);
+    expect(store.getFieldsByStatus(Statuses.PENDING, 0).length).toBe(0);
   });
 });

c9824b0c44922b03d4b66cddaefb47f985a33278
Herob527 <szymon.wrzos@onet.pl>
2024-11-17 22:23:07 +0100

    Added  tests

--- src/stores/newBindingsStore.test.ts
+++ src/stores/newBindingsStore.test.ts
@@ -29,9 +29,35 @@ describe('New bindings Store', () => {
       page: 0,
     });
 
-    store.removeFile(Statuses.PENDING, 'test');
+    store.removeFile('test');
 
     expect(store.getAll.length).toBe(0);
     expect(store.getFieldsByStatus(Statuses.PENDING, 0).length).toBe(0);
+  });
+  test('Update status', () => {
+    const store = useBindingsStoreV2();
+    store.addFile({
+      id: 'test',
+      duration: 5,
+      file: new File([], 'Test'),
+      status: Statuses.PENDING,
+      page: 0,
+    });
+    store.updateStatus('test', Statuses.IN_DB);
+
+    expect(store.getFieldsByStatus(Statuses.PENDING).length).toBe(0);
+    expect(store.getFieldsByStatus(Statuses.IN_DB).length).toBe(1);
+  });
+  test('Update file data', () => {
+    const store = useBindingsStoreV2();
+    store.addFile({
+      id: 'test',
+      duration: 5,
+      file: new File([], 'Test'),
+      status: Statuses.PENDING,
+      page: 0,
+    });
+    store.updateFileData('test', { duration: 10 });
+    expect(store.getFieldById('test').value?.duration).toBe(10);
   });
 });

881ca1ad2f7f1b336a187147819b1653b0028984
Herob527 <szymon.wrzos@onet.pl>
2025-01-03 09:24:56 +0100

    Added trying again later + whitespace and formatting

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -16,16 +16,18 @@ const { data } = useQuery({
   queryKey: ['bindingsCount'],
   queryFn,
 });
+
 </script>
 
 <template>
   <div
-    class="flex absolute top-1/2 left-1/2 flex-col justify-center items-center mb-4 w-screen h-screen text-center bg-gray-500 bg-opacity-50 -translate-x-1/2 -translate-y-1/2"
     v-if="data === null"
+    class="flex absolute top-1/2 left-1/2 flex-col justify-center items-center mb-4 w-screen h-screen text-center bg-gray-500 bg-opacity-50 -translate-x-1/2 -translate-y-1/2"
   >
     <p class="py-4 px-8 font-bold text-red-500 bg-gray-300">
       There was error while fetching bindings count.
     </p>
+    <p>Try again later</p>
   </div>
   <div class="flex flex-row flex-wrap gap-4 justify-center">
     <p>Welcome</p>

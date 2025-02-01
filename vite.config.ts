595a64b9000f7ba4b946d772e83c38ba8975004c
Herob527 <szymon.wrzos@onet.pl>
2025-02-01 12:05:33 +0100

    Migrated to tailwindcss v4

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,13 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import svgLoader from 'vite-svg-loader';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), svgLoader()],
+  plugins: [vue(), svgLoader(),
+  tailwindcss(),
+  ],
   server: {
     open: '/startup',
   },

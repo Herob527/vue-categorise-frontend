742a73336412f174268fb8cc20732ea7788e3e7f
Herob527 <szymon.wrzos@onet.pl>
2025-02-01 20:28:01 +0100

    Replaced prettier configh with mjs

---
+++ prettier.config.mjs
@@ -0,0 +1,13 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+export default {
+  semi: true,
+  tabWidth: 2,
+  singleQuote: true,
+  printWidth: 80,
+  trailingComma: 'all',
+  bracketSameLine: true,
+  singleAttributePerLine: true,
+};

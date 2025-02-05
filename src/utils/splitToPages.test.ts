dbc4c148eee2acf0e24f2a1a515540d87472d760
Herob527 <szymon.wrzos@onet.pl>
2025-02-05 22:43:05 +0100

    Removed pletora of test

--- src/utils/splitToPages.test.ts
+++ src/utils/splitToPages.test.ts
@@ -5,78 +5,10 @@ const tenPagesAmounts = Array(10)
   .fill(0)
   .map((_, index) => index + 91);
 
-const elevenPagesAmounts = Array(10)
-  .fill(0)
-  .map((_, index) => index + 101);
 
-tenPagesAmounts.forEach((amountOfEntries) => {
-  describe(`Split to pages utility - ${amountOfEntries} entries - pageSize 10`, () => {
-    test('User on 1 page', () => {
-      const pages = splitToPages({ amountOfEntries, selectedPage: 0 });
-      expect(pages).toEqual([0, 1, 2, 'dot', 10]);
-    });
+test('Split to pages utility - 309 entries - pageSize 20', () => {
 
-    test('User on 2 page', () => {
-      const pages = splitToPages({ amountOfEntries, selectedPage: 1 });
-      expect(pages).toEqual([0, 1, 2, 'dot', 10]);
-    });
-
-    test('User on 3 page', () => {
-      const pages = splitToPages({ amountOfEntries, selectedPage: 2 });
-      expect(pages).toEqual([0, 1, 2, 3, 'dot', 10]);
-    });
-    test('User on 4 page', () => {
-      const pages = splitToPages({ amountOfEntries, selectedPage: 3 });
-      expect(pages).toEqual([0, 'dot', 2, 3, 4, 'dot', 10]);
-    });
-
-    test('User on 8 page', () => {
-      const pages = splitToPages({ amountOfEntries, selectedPage: 7 });
-      expect(pages).toEqual([0, 'dot', 6, 7, 8, 'dot', 10]);
-    });
-    test('User on 9 page', () => {
-      const pages = splitToPages({ amountOfEntries, selectedPage: 8 });
-      expect(pages).toEqual([0, 'dot', 7, 8, 9, 10]);
-    });
-    test('User on last page', () => {
-      const pages = splitToPages({ amountOfEntries, selectedPage: 9 });
-      expect(pages).toEqual([0, 'dot', 8, 9, 10]);
-    });
-  });
-});
-
-elevenPagesAmounts.forEach((amountOfEntries) => {
-  describe(`Split to pages utility - ${amountOfEntries} entries - pageSize 10`, () => {
-    test('User on 1 page', () => {
-      const pages = splitToPages({ amountOfEntries, selectedPage: 0 });
-      expect(pages).toEqual([0, 1, 2, 'dot', 11]);
-    });
-
-    test('User on 2 page', () => {
-      const pages = splitToPages({ amountOfEntries, selectedPage: 1 });
-      expect(pages).toEqual([0, 1, 2, 'dot', 11]);
-    });
-
-    test('User on 3 page', () => {
-      const pages = splitToPages({ amountOfEntries, selectedPage: 2 });
-      expect(pages).toEqual([0, 1, 2, 3, 'dot', 11]);
-    });
-    test('User on 4 page', () => {
-      const pages = splitToPages({ amountOfEntries, selectedPage: 3 });
-      expect(pages).toEqual([0, 'dot', 2, 3, 4, 'dot', 11]);
-    });
-
-    test('User on 9 page', () => {
-      const pages = splitToPages({ amountOfEntries, selectedPage: 8 });
-      expect(pages).toEqual([0, 'dot', 7, 8, 9, 'dot', 11]);
-    });
-    test('User on 10 page', () => {
-      const pages = splitToPages({ amountOfEntries, selectedPage: 9 });
-      expect(pages).toEqual([0, 'dot', 8, 9, 10, 11]);
-    });
-    test('User on last page', () => {
-      const pages = splitToPages({ amountOfEntries, selectedPage: 10 });
-      expect(pages).toEqual([0, 'dot', 9, 10, 11]);
-    });
-  });
-});
+  const pages = splitToPages({ amountOfEntries: 309, selectedPage: 0, pageSize: 20 });
+  console.log(pages)
+  expect(pages).toEqual([0, 1, 2, 'dot', 15])
+})

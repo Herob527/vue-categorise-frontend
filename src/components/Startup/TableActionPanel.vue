a499f22bd3ed11205e243368d4c4e0ec8066cedf
Herob527 <szymon.wrzos@onet.pl>
2025-02-05 22:41:45 +0100

    Formatting

--- src/components/Startup/TableActionPanel.vue
+++ src/components/Startup/TableActionPanel.vue
@@ -10,6 +10,7 @@ const deleteAll = () => {
 };
 
 const handleFileUpload = (event: Event) => {
+  console.log('file-upload');
   const files = (event.target as HTMLInputElement)?.files;
   if (!files) return;
   emit('upload', files);
@@ -34,39 +35,32 @@ const emit = defineEmits<{
       <ActionButton
         :on-click="() => inputFileRef?.click()"
         class-name="bg-blue-500 text-white px-4 py-4 relative rounded-md hover:bg-blue-700"
-        label="Add item(s)"
-      >
+        label="Add item(s)">
         <input
           ref="inputFileRef"
           type="file"
           class="hidden"
           multiple
-          @change="handleFileUpload"
-        />
+          @change="handleFileUpload" />
         <font-awesome-icon
           :icon="faFile"
-          class="absolute top-1/2 left-1/2 w-1/2 h-1/2 text-white -translate-x-1/2 -translate-y-1/2"
-        />
+          class="absolute top-1/2 left-1/2 w-1/2 h-1/2 text-white -translate-x-1/2 -translate-y-1/2" />
       </ActionButton>
       <ActionButton
         :on-click="handleSubmitAll"
         :class-name="`${'bg-blue-500 hover:bg-blue-700 cursor-pointer'} text-white px-4 py-4 relative rounded-md`"
-        label="Submit all"
-      >
+        label="Submit all">
         <font-awesome-icon
           :icon="faPlus"
-          class="absolute top-1/2 left-1/2 w-1/2 h-1/2 text-white -translate-x-1/2 -translate-y-1/2"
-        />
+          class="absolute top-1/2 left-1/2 w-1/2 h-1/2 text-white -translate-x-1/2 -translate-y-1/2" />
       </ActionButton>
       <ActionButton
         :on-click="() => deleteAll()"
         class-name="bg-red-500 text-white px-4 py-4 relative rounded-md hover:bg-red-700"
-        label="Delete all"
-      >
+        label="Delete all">
         <font-awesome-icon
           :icon="faTrash"
-          class="absolute top-1/2 left-1/2 w-1/2 h-1/2 text-white -translate-x-1/2 -translate-y-1/2"
-        />
+          class="absolute top-1/2 left-1/2 w-1/2 h-1/2 text-white -translate-x-1/2 -translate-y-1/2" />
       </ActionButton>
     </div>
   </div>

ceb1c10ba35039069befc001cd8fd7bc2c6780a8
Herob527 <szymon.wrzos@onet.pl>
2025-02-08 22:57:54 +0100

    Replaced bg-opacity with backdrop (alternative design

--- src/components/ModalComponent.vue
+++ src/components/ModalComponent.vue
@@ -6,9 +6,8 @@ defineEmits(['close']);
 </script>
 <template>
   <div
-    class="fixed top-0 left-0 z-10 w-full h-full bg-black bg-opacity-50"
-    @click="$emit('close')"
-  ></div>
+    class="fixed top-0 left-0 z-10 w-full h-full backdrop-blur-sm backdrop-brightness-75"
+    @click="$emit('close')"></div>
   <div
     class="flex overflow-auto fixed top-1/2 left-1/2 z-10 flex-col p-2 pt-0 text-white -translate-x-1/2 -translate-y-1/2 max-h-[95vh] bg-primary-500">
     <div

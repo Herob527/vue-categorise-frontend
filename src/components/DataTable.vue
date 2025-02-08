db4f5f21da73664df62fb02a985adc9e017351ce
Herob527 <szymon.wrzos@onet.pl>
2025-02-08 22:24:36 +0100

    Removed not working jumping

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -21,7 +21,6 @@ defineEmits<{
 }>();
 
 const selectedPage = ref<number>(0);
-const pickedJumpPage = ref<number | null>(null);
 
 const pages = computed(() =>
   itemsCount.value
@@ -32,14 +31,6 @@ const pages = computed(() =>
       })
     : [],
 );
-
-const isValid = computed(() => {
-  if (!pickedJumpPage.value) return true;
-  return (
-    pickedJumpPage.value > 0 &&
-    pickedJumpPage.value < (props.itemsCount || 0) + 1
-  );
-});
 </script>
 <template>
   <div :class="props.className">
@@ -125,31 +116,11 @@ const isValid = computed(() => {
           v-else
           class="flex flex-col gap-2 w-10">
           <input
-            v-model="pickedJumpPage"
             type="text"
+            disabled
             inputmode="numeric"
             class="w-10 h-10 text-center border-2 border-primary-500"
             placeholder="..." />
-          <button
-            type="button"
-            class="inline-flex justify-center items-center w-10 h-10 text-white border-2 bg-primary-600 border-primary-500 hover:bg-primary-500"
-            @click="
-              () => {
-                console.log(pickedJumpPage, isValid);
-                if (!pickedJumpPage) return;
-                if (!isValid) return;
-
-                $emit('submit:page', pickedJumpPage);
-                selectedPage = pickedJumpPage;
-              }
-            ">
-            Go
-          </button>
-          <span
-            v-if="!isValid"
-            class="absolute right-1/2 top-full text-center text-red-500 translate-x-1/2">
-            Allowed range: {{ 1 }} - {{ props?.itemsCount || 1 }}
-          </span>
         </div>
       </template>
     </div>

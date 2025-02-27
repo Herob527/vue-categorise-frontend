557b616e494010e39ab36ac5ee573875246d606c
Herob527 <szymon.wrzos@onet.pl>
2025-02-27 23:24:31 +0100

    Adjusted config to be useful in docker

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,22 @@ import vue from '@vitejs/plugin-vue';
 import svgLoader from 'vite-svg-loader';
 import tailwindcss from '@tailwindcss/vite'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), svgLoader(),
   tailwindcss(),
   ],
   server: {
-    open: '/startup',
+    strictPort: true,
+    host: true,
+    open: process.env.DOCKER ? false : '/startup',
+    watch: {
+      usePolling: true,
+    },
+  },
+  preview: {
+    strictPort: true,
   },
   resolve: {
     alias: {

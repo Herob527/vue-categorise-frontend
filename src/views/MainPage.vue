c47df6ceb4d8df4e109190422fe1711aa87ed421
Herob527 <szymon.wrzos@onet.pl>
2025-02-27 23:23:50 +0100

    Formatting

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -21,8 +21,7 @@ const { data } = useQuery({
 <template>
   <div
     v-if="data === null"
-    class="flex absolute top-1/2 left-1/2 flex-col justify-center items-center mb-4 w-screen h-screen text-center bg-gray-500 bg-opacity-50 -translate-x-1/2 -translate-y-1/2"
-  >
+    class="flex absolute top-1/2 left-1/2 flex-col justify-center items-center mb-4 w-screen h-screen text-center bg-gray-500 bg-opacity-50 -translate-x-1/2 -translate-y-1/2">
     <p class="py-4 px-8 font-bold text-red-500 bg-gray-300">
       There was error while fetching bindings count.
     </p>

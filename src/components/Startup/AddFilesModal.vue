d1c91265efe574b216fb522df3c204a40ae0d4a9
Herob527 <szymon.wrzos@onet.pl>
2025-03-20 22:31:41 +0100

    Added tracking files and clearing category field

--- src/components/Startup/AddFilesModal.vue
+++ src/components/Startup/AddFilesModal.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { computed, ref, useTemplateRef } from 'vue';
 
-import { faFileImport } from '@fortawesome/free-solid-svg-icons';
+import { faFileImport, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 const fileInputRef = useTemplateRef<HTMLInputElement>('fileInput');
 
@@ -28,6 +28,7 @@ const isInCollection = computed(() => {
     (text) => text.toLowerCase() === input.value.trim().toLowerCase(),
   );
 });
+const files = ref<File[]>([]);
 </script>
 
 <template>
@@ -37,7 +38,15 @@ const isInCollection = computed(() => {
         ref="fileInput"
         type="file"
         class="hidden"
-        multiple />
+        multiple
+        @change="
+          (event) => {
+            const target = event.target as HTMLInputElement;
+            if (target.files) {
+              files.push(...Array.from(target.files));
+            }
+          }
+        " />
       <button
         class="hover:cursor-pointer flex items-center flex-col justify-center w-full bg-primary-500 text-white p-4 h-full hover:bg-primary-600 gap-4"
         @click="handleFileClick">
@@ -49,9 +58,19 @@ const isInCollection = computed(() => {
     </section>
     <section class="flex flex-col gap-2 p-2 border-4 border-primary-500 flex-1">
       <h2 class="text-xl font-bold">Category</h2>
-      <input
-        v-model="input"
-        class="p-2 border-primary-600 bg-white border-2 text-black" />
+      <div class="bg-white flex">
+        <input
+          v-model="input"
+          class="p-2 text-black w-full border-2 border-primary-600 border-r-0 outline-none" />
+        <button
+          type="button"
+          class="bg-red-500 text-white px-4 relative hover:cursor-pointer hover:bg-red-700 border-red-600 border-2 border-l-0"
+          @click="input = ''">
+          <font-awesome-icon
+            :icon="faXmark"
+            class="absolute top-1/2 left-1/2 w-1/2 h-1/2 text-white -translate-x-1/2 -translate-y-1/2" />
+        </button>
+      </div>
       <div
         v-if="dummyCategoryList.length > 0"
         class="flex flex-col gap-2 p-2 border-primary-600 bg-white border-2 text-white max-h-48 overflow-y-scroll">
@@ -84,7 +103,8 @@ const isInCollection = computed(() => {
       <button
         type="button"
         class="p-2 border-2 bg-primary-500 hover:bg-primary-600 cursor-pointer text-white">
-        Submit
+        <span v-if="files.length === 0"> Submit </span>
+        <span v-else> Submit ({{ files.length }} files)</span>
       </button>
     </section>
   </div>

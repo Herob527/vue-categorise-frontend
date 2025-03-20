81b41347c6af90f20e7500cd50cf6579ecf3378a
Herob527 <szymon.wrzos@onet.pl>
2025-03-20 22:55:11 +0100

    Added getting data from backend

--- src/components/Startup/AddFilesModal.vue
+++ src/components/Startup/AddFilesModal.vue
@@ -2,6 +2,8 @@
 import { computed, ref, useTemplateRef } from 'vue';
 
 import { faFileImport, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { useQuery } from '@tanstack/vue-query';
+import { getAll } from '@/actions/categories';
 
 const fileInputRef = useTemplateRef<HTMLInputElement>('fileInput');
 
@@ -9,26 +11,30 @@ const handleFileClick = () => {
   fileInputRef.value?.click();
 };
 
-const input = ref('');
+const categoryInput = ref('');
 
-const dummyCategoryList = computed(() =>
-  Array(20)
-    .fill('Category')
-    .map((text, index) => `${text} ${index}`)
-    .filter((text) =>
-      input.value.trim() === ''
-        ? true
-        : text.toLowerCase().includes(input.value.toLowerCase()),
-    ),
-);
+const { data, isLoading } = useQuery({
+  queryKey: ['category', 'get'],
+  queryFn: () => getAll(),
+});
 
 const isInCollection = computed(() => {
-  if (input.value === '') return false;
-  return dummyCategoryList.value.some(
-    (text) => text.toLowerCase() === input.value.trim().toLowerCase(),
+  if (categoryInput.value === '') return false;
+  return data.value?.some(
+    (text) =>
+      text.name.toLowerCase() === categoryInput.value.trim().toLowerCase(),
   );
 });
 const files = ref<File[]>([]);
+
+type ReturnData = {
+  files: File[];
+  category: string;
+};
+
+defineEmits<{
+  (e: 'submit', data: ReturnData): void;
+}>();
 </script>
 
 <template>
@@ -60,35 +66,42 @@ const files = ref<File[]>([]);
       <h2 class="text-xl font-bold">Category</h2>
       <div class="bg-white flex flex-row flex-nowrap">
         <input
-          v-model="input"
+          v-model="categoryInput"
           maxlength="50"
           class="p-2 text-black w-full border-2 border-primary-600 border-r-0 outline-none" />
         <button
           type="button"
           class="bg-red-500 text-white px-4 relative hover:cursor-pointer hover:bg-red-700 border-red-600 border-2 border-l-0"
-          @click="input = ''">
+          @click="categoryInput = ''">
           <font-awesome-icon
             :icon="faXmark"
             class="absolute top-1/2 left-1/2 w-1/2 h-1/2 text-white -translate-x-1/2 -translate-y-1/2" />
         </button>
       </div>
       <div
-        v-if="dummyCategoryList.length > 0"
-        class="flex flex-col gap-2 p-2 border-primary-600 bg-white border-2 text-white max-h-48 overflow-y-scroll">
-        <button
-          v-for="category in dummyCategoryList"
-          :key="category"
-          class="p-2 border-2 bg-primary-500 hover:bg-primary-600 cursor-pointer"
-          @click="input = category">
-          {{ category }}
-        </button>
+        class="flex flex-col gap-2 p-2 border-primary-600 bg-white border-2 max-h-48 overflow-y-scroll">
+        <template v-if="data && data.length > 0">
+          <button
+            v-for="category in data"
+            :key="category.name"
+            class="p-2 border-2 bg-primary-500 hover:bg-primary-600 cursor-pointer text-white"
+            @click="categoryInput = category.name">
+            {{ category }}
+          </button>
+        </template>
+        <p v-else-if="isLoading">Loading...</p>
+        <p
+          v-else
+          class="text-gray-500">
+          No categories found
+        </p>
       </div>
 
-      <template v-if="input.length > 0">
+      <template v-if="categoryInput.length > 0">
         <span v-if="isInCollection"
           >Category
           <code class="border-2 border-gray-500 p-1 rounded-md">{{
-            input.trim()
+            categoryInput.trim()
           }}</code>
           will be
           <b>reused</b></span
@@ -96,7 +109,7 @@ const files = ref<File[]>([]);
         <span v-else>
           Category
           <code class="border-2 border-gray-500 p-1 rounded-md">{{
-            input.trim()
+            categoryInput.trim()
           }}</code>
           will be <b>created</b></span
         >
@@ -104,7 +117,8 @@ const files = ref<File[]>([]);
       <span v-else> Files will be <b>uncategorized</b> </span>
       <button
         type="button"
-        class="p-2 border-2 bg-primary-500 hover:bg-primary-600 cursor-pointer text-white">
+        class="p-2 border-2 bg-primary-500 hover:bg-primary-600 cursor-pointer text-white"
+        @click="$emit('submit', { files, category: categoryInput })">
         <span v-if="files.length === 0"> Submit </span>
         <span v-else> Submit ({{ files.length }} files)</span>
       </button>

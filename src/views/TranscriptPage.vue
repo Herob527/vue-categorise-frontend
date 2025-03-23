a567c6f8beb6adccf6b8ffce8ec1904d8539531f
Herob527 <szymon.wrzos@onet.pl>
2025-03-23 16:12:34 +0100

    Reformat and set is options open to false

--- src/views/TranscriptPage.vue
+++ src/views/TranscriptPage.vue
@@ -6,7 +6,7 @@ import { faGear } from '@fortawesome/free-solid-svg-icons';
 import { ref } from 'vue';
 import OptionPanelContainer from '@/components/Transcript/OptionsPanel/OptionPanelContainer.vue';
 
-const isOptionsOpen = ref(true);
+const isOptionsOpen = ref(false);
 
 const handleOptionClick = () => {
   isOptionsOpen.value = true;
@@ -18,7 +18,9 @@ const closeHandler = () => {
 </script>
 <template>
   <div class="flex flex-row gap-4 px-2 pb-4 mx-auto">
-    <ActionButton :on-click="handleOptionClick" class-name="bg-blue-500 text-white px-4 py-2 rounded-xl">
+    <ActionButton
+      :on-click="handleOptionClick"
+      class-name="bg-blue-500 text-white px-4 py-2 rounded-xl">
       <font-awesome-icon :icon="faGear" />
       Settings
     </ActionButton>
@@ -26,6 +28,8 @@ const closeHandler = () => {
   <main class="flex flex-col flex-1 gap-3 px-2 pb-4 mx-auto">
     <TranscriptList />
     <PaginationContainer />
-    <OptionPanelContainer v-if="isOptionsOpen" @close="closeHandler" />
+    <OptionPanelContainer
+      v-if="isOptionsOpen"
+      @close="closeHandler" />
   </main>
 </template>

bbda6a992ea1440ce628a2e05f01b129856503ce
Herob527 <szymon.wrzos@onet.pl>
2025-04-05 00:10:11 +0200

    Working on dashboard

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -12,22 +12,14 @@ const queryFn = async () => {
   }
 };
 
-const { data } = useQuery({
+const { data, isLoading, isError } = useQuery({
   queryKey: ['bindingsCount'],
   queryFn,
 });
 </script>
 
 <template>
-  <div
-    v-if="data === null"
-    class="flex absolute top-1/2 left-1/2 flex-col justify-center items-center mb-4 w-screen h-screen text-center bg-gray-500 bg-opacity-50 -translate-x-1/2 -translate-y-1/2">
-    <p class="py-4 px-8 font-bold text-red-500 bg-gray-300">
-      There was error while fetching bindings count.
-    </p>
-    <p>Try again later</p>
-  </div>
-  <div class="flex flex-row flex-wrap gap-4 justify-center">
-    <p>Welcome</p>
-  </div>
+  <div v-if="isLoading">Loading</div>
+  <div v-else-if="isError">Error</div>
+  <div v-else>Bindings: {{ data }}</div>
 </template>

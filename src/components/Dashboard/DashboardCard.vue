3bd40bb6048af23cc1bbfeaddf1a1dc1b2851eb1
Herob527 <szymon.wrzos@onet.pl>
2025-05-25 00:33:56 +0200

    Added icon

--- src/components/Dashboard/DashboardCard.vue
+++ src/components/Dashboard/DashboardCard.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
-defineProps<{ title: string }>();
+import type { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+
+defineProps<{ title: string; icon: IconDefinition }>();
 </script>
 
 <template>
-  <div class="bg-primary-500 text-white max-w-64 max-h-48 drop-shadow-2xl">
-    <h2 class="text-2xl font-bold border-b-2 border-gray-100 p-4 py-2">
-      {{ title }}
-    </h2>
+  <div class="bg-primary-500 text-white max-w-96 max-h-48 drop-shadow-2xl">
+    <header
+      class="text-xl font-bold border-b-2 border-gray-100 p-4 py-2 flex flex-row justify-between gap-2 items-center">
+      <h2>
+        {{ title }}
+      </h2>
+      <font-awesome-icon :icon="icon" />
+    </header>
     <div class="p-4">
       <slot />
     </div>

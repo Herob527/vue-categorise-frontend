b07a020b31f9e0d1f9b028bc9eb2cf58ec13e687
Herob527 <szymon.wrzos@onet.pl>
2025-05-31 11:48:28 +0200

    Remove import

--- src/views/TranscriptPage.vue
+++ src/views/TranscriptPage.vue
@@ -5,7 +5,7 @@ import ActionButton from '@/components/ActionButton.vue';
 import PaginationContainer from '@/components/Transcript/Pagination/PaginationContainer.vue';
 import TranscriptList from '@/components/Transcript/TranscriptList.vue';
 import { faGear } from '@fortawesome/free-solid-svg-icons';
-import { ref, watchEffect } from 'vue';
+import { ref } from 'vue';
 import OptionPanelContainer from '@/components/Transcript/OptionsPanel/OptionPanelContainer.vue';
 
 import { getPaginated } from '@/actions/bindings';
@@ -63,8 +63,6 @@ const handleNewPage = (newPage: number) => {
     },
   });
 };
-
-watchEffect(() => {});
 </script>
 <template>
   <div class="flex flex-row gap-4 px-2 pb-4 mx-auto">

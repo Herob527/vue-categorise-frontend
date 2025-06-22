ef79ea23b0f23ea075f7192814934958fbf1eeaf
Herob527 <szymon.wrzos@onet.pl>
2025-06-22 16:26:48 +0200

    Remove base prop

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.DOCKER ? '/web' : './',
   plugins: [
     vue(),
     svgLoader(),

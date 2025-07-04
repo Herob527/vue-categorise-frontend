0d97da891411bb70e664ce7d987cb90e85f9fe1d
Herob527 <szymon.wrzos@onet.pl>
2025-07-04 22:55:51 +0200

    Remove redudant component

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-import 'vue-virtual-scroller/dist/vue-virtual-scroller.css';
 import App from './App.vue';
 import router from './router';
 import { VueQueryPlugin } from '@tanstack/vue-query';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { config } from '@fortawesome/fontawesome-svg-core';
-import VueVirtualScroller from 'vue-virtual-scroller';
 import { createI18n } from 'vue-i18n';
 import enUs from './locale/en-US.json';
 import type { MessageSchema } from './locale/schema';
@@ -22,7 +20,6 @@ const i18n = createI18n<{
   },
 });
 
-app.use(VueVirtualScroller);
 app.use(i18n);
 
 config.styleDefault = 'solid';

d3cd54101baf7402105d2b5be414fcbf8c1f7ccb
Herob527 <szymon.wrzos@onet.pl>
2025-08-15 18:38:27 +0200

    Add basic downloading link

--- src/components/Finalise/RealDataPreview/PreviewContainer.vue
+++ src/components/Finalise/RealDataPreview/PreviewContainer.vue
@@ -2,13 +2,33 @@
 import { useFinaliseRealPreviewStore } from '@/stores/finalisationDataStore';
 import DirectoryItem from '../Preview/DirectoriesPreview/DirectoryItem.vue';
 import FileIcon from '../Preview/DirectoriesPreview/FileIcon.vue';
+import { useMutation } from '@tanstack/vue-query';
+import { getLink } from '@/actions/finalise';
 
 const store = useFinaliseRealPreviewStore();
+const { mutateAsync } = useMutation({
+  mutationFn: (objectName: string) => {
+    return getLink(objectName);
+  },
+});
 </script>
 <template>
   <section
     class="container flex overflow-scroll flex-col rounded-xl border-2 h-min max-h-[500px] border-primary-500">
-    <h2 class="px-2 my-2 text-2xl font-bold">Folder preview (Real)</h2>
+    <header class="flex justify-between flex-row p-2">
+      <h2 class="text-2xl font-bold">Folder preview (Real)</h2>
+      <button
+        type="button"
+        class="text-white px-4 py-2 bg-primary-500 hover:bg-primary-600 rounded-full"
+        @click="
+          async () => {
+            const link = await mutateAsync('temp');
+            console.log(link);
+          }
+        ">
+        Download
+      </button>
+    </header>
     <div class="flex flex-col flex-wrap gap-3 px-2 pb-2">
       <template v-if="store.processedData">
         <template

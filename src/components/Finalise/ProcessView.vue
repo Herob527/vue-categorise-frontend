f15e379774eba724f140bb90718c992fb7d71ba1
Herob527 <szymon.wrzos@onet.pl>
2026-04-04 22:20:38 +0200

    Added basic view

---
+++ src/components/Finalise/ProcessView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import finalize from '@/actions/finalise';
+import { useQuery } from '@tanstack/vue-query';
+
+const { data } = useQuery({
+  queryKey: ['finalize-get-all'],
+  queryFn: () => {
+    return finalize.getAll();
+  },
+});
+</script>
+
+<template>
+  <div>
+    <p>{{ data }}</p>
+  </div>
+</template>

5a7f96125094eb571a7ca68fac0255d4f34ab769
Herob527 <szymon.wrzos@onet.pl>
2026-04-05 08:53:00 +0200

    Working on date

--- src/components/Finalise/ProcessView.vue
+++ src/components/Finalise/ProcessView.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import finalize from '@/actions/finalise';
 import { useQuery } from '@tanstack/vue-query';
+import DataTable from '@/components/DataTable.vue';
+
+import ActionButton from '@/components/ActionButton.vue';
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
 
 const { data } = useQuery({
   queryKey: ['finalize-get-all'],
@@ -17,23 +21,77 @@ const handleClick = async (exportId: string) => {
   a.click();
   URL.revokeObjectURL(url);
 };
+const processDate = (date: string) => {
+  console.log(Temporal.PlainDateTime.from(date));
+  return Temporal.PlainDateTime.from(date).toString();
+};
+
+const fields = ['Id', 'Created at', 'Updated at', 'Status', 'Actions'];
 </script>
 
 <template v-if="data && Array.isArray(data)">
-  <div
-    v-for="item in data"
-    :key="item.id"
-    class="grid grid-cols-4 gap-4 items-center justify-center">
-    <span>{{ item.created_at }}</span>
-    <span>{{ item.updated_at }}</span>
-    <span>{{ item.status }}</span>
-    <button
-      type="button"
-      class="p-4 rounded-xl relative bg-primary-500 w-fit hover:bg-primary-600 cursor-pointer text-white flex"
-      @click="() => handleClick(item.id)">
-      <span class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        P
+  <DataTable
+    :data="data ?? []"
+    :item-keys="fields"
+    :class-name="`rounded-xl border-2 border-primary-500 overflow-clip w-full`"
+    :page-size="1200"
+    :items-count="data?.length"
+    :page="0"
+    :pagination-key="''">
+    <template #top-heading>
+      <div class="flex flex-row bg-primary-600 p-2 justify-between">
+        <p class="text-2xl font-bold text-white uppercase">Processes</p>
+      </div>
+    </template>
+    <template #fallback>
+      <span class="p-4">Nothing there yet</span>
+    </template>
+
+    <template #item="{ index, entry }">
+      <div
+        :class="[
+          'w-full text-center',
+          index % 2 == 0
+            ? 'bg-gray-200 hover:bg-gray-300'
+            : 'hover:bg-gray-100',
+          'flex flex-row justify-center items-center py-2',
+        ]">
+        <div class="flex-1">
+          <span
+            class="py-1 px-2 rounded-lg cursor-text"
+            :title="entry.id">
+            {{ entry.id }}
+          </span>
+        </div>
+
+        <div class="flex-1">
+          {{ processDate(entry.created_at) }}
+        </div>
+
+        <div class="flex-1">
+          {{ processDate(entry.updated_at) }}
+        </div>
+
+        <div class="flex-1">
+          {{ entry.status }}
+        </div>
+        <div class="flex-1">
+          <ActionButton
+            :on-click="() => handleClick(entry.id)"
+            class-name="bg-blue-500 text-white px-4 py-4 relative rounded-md hover:bg-blue-700 "
+            label="Delete">
+            <font-awesome-icon
+              :icon="faDownload"
+              class="absolute top-1/2 left-1/2 w-1/2 h-1/2 text-white -translate-x-1/2 -translate-y-1/2" />
+          </ActionButton>
+        </div>
+      </div>
+    </template>
+    <template #loadingItem="{ index }">
+      <span
+        :class="`w-full ${index % 2 == 1 ? 'bg-gray-200 hover:bg-gray-300' : 'hover:bg-gray-100'} h-[50px] animate-pulse`">
+        &nbsp;
       </span>
-    </button>
-  </div>
+    </template>
+  </DataTable>
 </template>

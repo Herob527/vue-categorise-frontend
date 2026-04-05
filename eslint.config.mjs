922a81088f3351d8414ab7101cca0977e9f14264
Herob527 <szymon.wrzos@onet.pl>
2026-04-05 11:46:11 +0200

    Adjustments of liner

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,8 @@
-/* eslint-env node */
 import eslint from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import eslintPluginVue from 'eslint-plugin-vue';
 import globals from 'globals';
 import typescriptEslint from 'typescript-eslint';
-
 
 export default typescriptEslint.config(
   { ignores: ['*.d.ts', '**/coverage', '**/dist', 'src/types/generated/**/*'] },
@@ -13,19 +11,17 @@ export default typescriptEslint.config(
       eslint.configs.recommended,
       ...typescriptEslint.configs.strict,
       ...eslintPluginVue.configs['flat/recommended'],
+      eslintConfigPrettier,
     ],
     files: ['**/*.{ts,vue}'],
     languageOptions: {
       ecmaVersion: 'latest',
       sourceType: 'module',
       globals: globals.browser,
-      parserOptions: {
-        parser: typescriptEslint.parser,
-      },
+      parser: typescriptEslint.parser,
     },
     rules: {
       // your rules
     },
   },
-  eslintConfigPrettier
 );

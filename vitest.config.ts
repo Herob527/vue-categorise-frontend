b67680b4c65349632aa455dc070e015abb933024
Herob527 <szymon.wrzos@onet.pl>
2026-04-26 22:35:16 +0200

    Remove redundant field

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,7 @@
-import { fileURLToPath } from 'node:url'
-import { mergeConfig } from 'vite'
-import { configDefaults, defineConfig } from 'vitest/config'
-import viteConfig from './vite.config'
+import { fileURLToPath } from 'node:url';
+import { mergeConfig } from 'vite';
+import { configDefaults, defineConfig } from 'vitest/config';
+import viteConfig from './vite.config';
 
 export default mergeConfig(
   viteConfig,
@@ -10,9 +10,6 @@ export default mergeConfig(
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/*'],
       root: fileURLToPath(new URL('./', import.meta.url)),
-      transformMode: {
-        web: [/\.[jt]sx$/]
-      }
-    }
-  })
-)
+    },
+  }),
+);

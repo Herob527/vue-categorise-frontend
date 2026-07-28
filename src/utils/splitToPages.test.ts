e8b509f9343ae45d05f0c377a151fce0d075b739
Herob527 <szymon.wrzos@onet.pl>
2026-07-28 19:38:33 +0200

    Added more cases

--- src/utils/splitToPages.test.ts
+++ src/utils/splitToPages.test.ts
@@ -9,3 +9,21 @@ test('Split to pages utility - 309 entries - pageSize 20', () => {
   });
   expect(pages).toEqual([0, 1, 2, 'dot', 15]);
 });
+
+test('Split to pages utility - 300 entries - pageSize 20', () => {
+  const pages = splitToPages({
+    amountOfEntries: 300,
+    selectedPage: 0,
+    pageSize: 20,
+  });
+  expect(pages).toEqual([0, 1, 2, 'dot', 15]);
+});
+
+test('Split to pages utility - 299 entries - pageSize 20', () => {
+  const pages = splitToPages({
+    amountOfEntries: 299,
+    selectedPage: 0,
+    pageSize: 20,
+  });
+  expect(pages).toEqual([0, 1, 2, 'dot', 14]);
+});

b4f4ba80234a599fda8982f6b3ae67b7642d6538
Herob527 <szymon.wrzos@onet.pl>
2026-07-28 19:53:18 +0200

    Remvoe unused fules

--- src/utils/generateId.ts
+++
@@ -1,4 +0,0 @@
-export const generateId = (file: File) => {
-  const { name, size } = file;
-  return `${name}-${size}`;
-};
--- src/utils/isInRange.ts
+++
@@ -1,3 +0,0 @@
-export const isInRange = (value: number, min: number, max: number): boolean => {
-  return value >= min && value <= max;
-};

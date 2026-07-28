d5744e3ed5dd463238d9717d7e7e08aa1cffcc66
Herob527 <szymon.wrzos@onet.pl>
2026-07-28 19:41:03 +0200

    Remove redundant file

--- src/axiosConfig.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-import { API_URL } from './constants';
-
-const axiosApi = axios.create({
-  baseURL: API_URL,
-  headers: { 'Content-Type': 'application/json' },
-});
-axiosApi.defaults.baseURL = API_URL;
-export { axiosApi };

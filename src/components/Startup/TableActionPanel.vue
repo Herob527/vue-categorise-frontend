27ca68cd88abcae85ce43d5aa2e89295e51a7941
Herob527 <szymon.wrzos@onet.pl>
2026-07-28 20:39:21 +0200

    Add actual labels by icons

--- src/components/Startup/TableActionPanel.vue
+++ src/components/Startup/TableActionPanel.vue
@@ -29,30 +29,36 @@ const emit = defineEmits<{
     <div class="flex flex-row gap-2 p-2 w-full">
       <ActionButton
         :on-click="() => $emit('uploadClick')"
-        class-name="bg-blue-500 text-white px-4 py-4 relative rounded-md hover:bg-blue-700 "
+        class-name="bg-blue-500 text-white px-4 py-2 relative rounded-md hover:bg-blue-700 "
         :disabled="disabledButtons.includes('UPLOAD')"
         label="Add item(s)">
-        <font-awesome-icon
-          :icon="faFile"
-          class="absolute top-1/2 left-1/2 w-1/2 h-1/2 text-white -translate-x-1/2 -translate-y-1/2" />
+        <div class="text-white">
+          <font-awesome-icon :icon="faFile" />
+
+          <span> Add items </span>
+        </div>
       </ActionButton>
       <ActionButton
         :on-click="handleSubmitAll"
-        :class-name="`${'bg-blue-500 hover:bg-blue-700 cursor-pointer'} text-white px-4 py-4 relative rounded-md disabled:bg-gray-400`"
+        :class-name="`${'bg-blue-500 hover:bg-blue-700 cursor-pointer'} text-white px-4 py-2 relative rounded-md disabled:bg-gray-400`"
         label="Submit all"
         :disabled="disabledButtons.includes('SUBMIT')">
-        <font-awesome-icon
-          :icon="faPlus"
-          class="absolute top-1/2 left-1/2 w-1/2 h-1/2 text-white -translate-x-1/2 -translate-y-1/2" />
+        <div class="text-white">
+          <font-awesome-icon :icon="faPlus" />
+
+          <span> Submit items </span>
+        </div>
       </ActionButton>
       <ActionButton
         :on-click="() => deleteAll()"
-        class-name="bg-red-500 text-white px-4 py-4 relative rounded-md hover:bg-red-700 disabled:bg-gray-400"
+        class-name="bg-red-500 text-white px-4 py-2 relative rounded-md hover:bg-red-700 disabled:bg-gray-400"
         label="Delete all"
         :disabled="disabledButtons.includes('DELETE')">
-        <font-awesome-icon
-          :icon="faTrash"
-          class="absolute top-1/2 left-1/2 w-1/2 h-1/2 text-white -translate-x-1/2 -translate-y-1/2" />
+        <div class="text-white">
+          <font-awesome-icon :icon="faTrash" />
+
+          <span> Delete items </span>
+        </div>
       </ActionButton>
     </div>
   </div>
